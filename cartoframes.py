ad50bfcf78c263a427a21575b3760faca35c0d95
Andy Eschbacher <andy.eschbacher@gmail.com>
2017-02-16 14:49:59 -0500

    adds geomtype fetcher for use in metadata

--- cartoframes.py
+++ cartoframes.py
@@ -1,11 +1,11 @@
 """
-Monkey patching pandas to add utilities for CARTO tables
+Monkey patching pandas to add utilities for CARTO tables and maps
 Andy Eschbacher and Stuart Lynn, 2017
 
 Project goals
     * Interact with a CARTO table fully within a Jupyter notebook/pandas
-      workflow
-    * Like geopandas, have a .cartomap() method which gives back the data
+      workflow (read and sync dataframe changes)
+    * Like geopandas, have a .carto_map() method which gives back the data
       as a map using carto's maps api and turbocartocss on an optional
       attribute
     * Add CARTO services like the Data Observatory as methods to a dataframe
@@ -30,6 +30,7 @@ Notes on propagating pandas metadata:
 import pandas as pd
 import cartodb
 
+# not currently used
 def add_meta(self, **kwargs):
     """
         Set metadata for a dataframe if none has been already set
@@ -58,10 +59,31 @@ def transform_schema(pgschema):
             continue
         datatypes[field] = map_dtypes(pgschema[field]['type'])
     return datatypes
-# NOTE: this is compatible with current version of carto-python client
-# https://github.com/CartoDB/carto-python/blob/584da36530292ca252db1f05ddf36a9cc8464ecb/README.md#using-api-key
+
+def get_geom_type(sql_auth_client, tablename):
+    """
+        Get the geometry type in tablename for storing in
+        dataframe metadata
+    """
+    geomtypes = {'ST_Point': 'point',
+                 'ST_MultiPoint': 'point',
+                 'ST_LineString': 'line',
+                 'ST_MultiLineString': 'line',
+                 'ST_Polygon': 'polygon',
+                 'ST_MultiPolygon': 'polygon'}
+
+    result = sql_auth_client.send('''
+        SELECT ST_GeometryType(the_geom) As geomtype
+        FROM "{tablename}"
+        LIMIT 1'''.format(tablename=tablename))['rows'][0]['geomtype']
+
+    return geomtypes[result]
+
+# NOTE: this is compatible with v1.0.0 of carto-python client
+# TODO: remove username as a param would be nice.. accessible to write to
+#       metadata from carto python client?
 def read_carto(cdb_client, username=None, tablename=None,
-               custom_query=None, api_key=None, include_geom=True,
+               custom_query=None, include_geom=True,
                limit=None, index='cartodb_id', debug=False):
     """Import a table from carto into a pandas dataframe, storing
        table information in pandas metadata"""
@@ -72,6 +94,8 @@ def read_carto(cdb_client, username=None, tablename=None,
     # construct query
     if tablename:
         query = 'SELECT * FROM "{tablename}"'.format(tablename=tablename)
+        geomtype = get_geom_type(sql, tablename)
+        # Add limit if requested
         if limit:
             # NOTE: what if limit is `all` or `none`?
             if (limit >= 0) and isinstance(limit, int):
@@ -79,7 +103,9 @@ def read_carto(cdb_client, username=None, tablename=None,
             else:
                 raise ValueError("`limit` parameter must an integer >= 0")
     elif query:
-        query = custom_query
+        # NOTE: note yet implemented
+        # query = custom_query
+        pass
     else:
         raise NameError("`tablename` or `query` needs to be specified")
 
@@ -105,7 +131,8 @@ def read_carto(cdb_client, username=None, tablename=None,
                                      'carto_api_key': api_key,
                                      'carto_include_geom': include_geom,
                                      'carto_limit': limit,
-                                     'carto_schema': str(_df.columns)}))
+                                     'carto_schema': str(schema),
+                                     'carto_geomtype': geomtype}))
     _df.carto_last_state = _df.copy(deep=True)
     _df.carto_sql_client = sql
     return _df
@@ -152,12 +179,10 @@ def update_carto(self, createtable=False, debug=False):
         raise Exception("Table not registered with CARTO. Set `createtable` "
                         "flag to True")
 
-    last_state = self.carto_last_state
-
     # create new column if needed
     # TODO: extract to function
-    if len(set(self.columns) - set(last_state.columns)) > 0:
-        newcols = set(self.columns) - set(last_state.columns)
+    if len(set(self.columns) - set(self.carto_last_state.columns)) > 0:
+        newcols = set(self.columns) - set(self.carto_last_state.columns)
         for col in newcols:
             if debug: print "Create new column {col}".format(col=col)
             alter_query = '''
@@ -174,7 +199,7 @@ def update_carto(self, createtable=False, debug=False):
             for item in self[col].iteritems():
                 if debug: print item
                 update_query = '''
-                    UPDATE {tablename}
+                    UPDATE "{tablename}"
                     SET "{colname}" = {colval}
                     WHERE "cartodb_id" = {cartodb_id};
                 '''.format(tablename=json.loads(self._metadata[0])['carto_table'],
@@ -186,8 +211,8 @@ def update_carto(self, createtable=False, debug=False):
                 # if debug: print resp.text
     # drop column if needed
     # TODO: extract to function
-    if len(set(last_state.columns) - set(self.columns)) > 0:
-        discardedcols = set(last_state.columns) - set(self.columns)
+    if len(set(self.carto_last_state.columns) - set(self.columns)) > 0:
+        discardedcols = set(self.carto_last_state.columns) - set(self.columns)
         for col in discardedcols:
             alter_query = '''
                 ALTER TABLE "{tablename}"
@@ -198,9 +223,9 @@ def update_carto(self, createtable=False, debug=False):
             if debug: print alter_query
             resp = self.carto_sql_client.send(alter_query)
     # sync updated values
-    # TODO: extract to column
-    common_cols = list(set(self.columns) & set(last_state.columns))
-    df_diff = (self[common_cols] != last_state[common_cols]).stack()
+    # TODO: extract to functon
+    common_cols = list(set(self.columns) & set(self.carto_last_state.columns))
+    df_diff = (self[common_cols] != self.carto_last_state[common_cols]).stack()
     for i in df_diff.iteritems():
         # TODO: instead of doing row by row, build up a list of queries
         #       testing to be sure the num of characters is lower than
@@ -210,7 +235,7 @@ def update_carto(self, createtable=False, debug=False):
             cartodb_id = i[0][0]
             colname = i[0][1]
             upsert_query = '''
-            INSERT INTO {tablename}("cartodb_id", "{colname}")
+            INSERT INTO "{tablename}"("cartodb_id", "{colname}")
                  VALUES ({cartodb_id}, {colval})
             ON CONFLICT ("cartodb_id")
             DO UPDATE SET "{colname}" = {colval}
@@ -220,24 +245,31 @@ def update_carto(self, createtable=False, debug=False):
                        colval=process_item(self.loc[cartodb_id][colname]),
                        cartodb_id=cartodb_id)
             if debug: print upsert_query
-            # TODO: replace with carto-python client
             resp = self.carto_sql_client.send(upsert_query)
             if debug: print json.loads(resp.text)
         else:
             continue
+    # update state of dataframe
+    self.carto_last_state = self.copy(deep=True)
 
 pd.DataFrame.update_carto = update_carto
+
 
 def carto_map(self, interactive=True, stylecol=None):
     """
         Produce and return CARTO maps or iframe embeds
     """
+    import urllib
+    import json
     try:
         import IPython
         return_iframe = False
-    except:
+    except ImportError:
         return_iframe = True
-    import urllib
+
+    if interactive is False:
+        # TODO: use carto-python client to create static map?
+        pass
 
     if stylecol:
         fill_style = ('ramp([{stylecol}], '
@@ -249,7 +281,7 @@ def carto_map(self, interactive=True, stylecol=None):
     df_meta = json.loads(self._metadata[-1])
     params = {'username': df_meta['carto_username'],
               'tablename': df_meta['carto_table'],
-               'fill_style': fill_style}
+              'fill_style': fill_style}
     mapconfig = '''{"user_name": "%(username)s",
                     "type": "cartodb",
                     "sublayers": [{
@@ -261,7 +293,7 @@ def carto_map(self, interactive=True, stylecol=None):
                       }],
                       "subdomains": [ "a", "b", "c" ]
                       }''' % params
-    params = dict({'q': urllib.quote(mapconfig)}, **params)
+    params['q'] = urllib.quote(mapconfig)
     # print params
     url = '?'.join(['/files/cartoframes.html',
                     urllib.urlencode(params)])
