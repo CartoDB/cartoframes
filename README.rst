25da5074a0387871f7d40c52be4e898aa9777080
Andy Eschbacher <andy.eschbacher@gmail.com>
2017-02-27 15:39:16 -0500

    moving to pip structure

---
+++ README.rst
@@ -0,0 +1,44 @@
+CartoFrames
+===========
+
+A pandas interface for integrating `Carto <https://carto.com/>`__ into a
+data science workflow.
+
+Example usage
+-------------
+
+Data workflow
+~~~~~~~~~~~~~
+
+Get table from carto, make changes in pandas, sync updates with carto:
+
+.. code:: python
+
+    import pandas as pd
+    import carto
+    import cartoframes
+
+    cdb_auth_client = carto.auth.APIAuthClient(base_url, APIKEY)
+
+    df = pd.read_carto(cdb_auth_client, 'tablename')
+    # do fancy pandas operations (add/drop columns, change values, etc.)
+    df.sync_carto() # updates carto table with all changes from this session
+
+Map workflow
+~~~~~~~~~~~~
+
+The following will embed a CARTO map in a Jupyter notebook (interactive
+or static).
+
+.. code:: python
+
+    df = pd.read_carto(auth_client, 'tablename')
+    df.carto_map(interactive=True, stylecol='interesting_col')
+
+.. figure:: cartoframe-map-example.png
+   :alt: 
+
+Augment from Data Observatory
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Not yet implemented
---
+++ setup.py
@@ -0,0 +1,69 @@
+"""
+cartoframes
+"""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+# TODO: fill this in
+packages = [
+    'pandas',
+]
+
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='cartoframes',
+    version='0.0.1',
+    description='An experimental Python pandas interface for using CARTO',
+    long_description=long_description,
+    url='https://github.com/CartoDB/cartoframes',
+    author='Andy Eschbacher',
+    author_email='andy@carto.com',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+    keywords='data science maps spatial pandas',
+    packages=['cartoframes'],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[
+        'https://github.com/CartoDB/carto-python.git@e404d9a96afcd5cdd72cadf8e5461c3e67fd6d76#egg=carto',
+        'pandas'
+    ],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    # extras_require={
+    #     'dev': ['check-manifest'],
+    #     'test': ['coverage'],
+    # },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    package_data={
+        '': ['LICENSE'],
+    },
+)
