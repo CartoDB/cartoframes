6234b782416ae9c841155a41c39ac49c18d820a4
Andy Eschbacher <andy.eschbacher@gmail.com>
2017-03-10 12:00:49 -0500

    bumps patch number

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='cartoframes',
-    version='0.1.1',
+    version='0.1.2',
     description='An experimental Python pandas interface for using CARTO',
     long_description=long_description,
     url='https://github.com/CartoDB/cartoframes',
