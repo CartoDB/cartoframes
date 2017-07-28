6be87e0362f24c8858273c2bf1a3ce091bfc477c
Andy Eschbacher <andy.eschbacher@gmail.com>
2017-07-28 16:45:10 -0400

    tests for cartocontext instance

--- test/test_context.py
+++ test/test_context.py
@@ -1,20 +1,38 @@
 """Unit tests for cartoframes.layers"""
 import unittest
 import os
+import json
 import cartoframes
 from carto.exceptions import CartoException
-
+from carto.auth import APIKeyAuthClient
+from carto.sql import SQLClient
 
 class TestCartoContext(unittest.TestCase):
     """Tests for cartoframes.CartoContext"""
     def setUp(self):
-        self.apikey = os.environ["APIKEY"]
-        self.username = os.environ["USERNAME"]
+        creds = json.loads(open('test/secret.json').read())
+        self.apikey = os.environ.get('APIKEY', creds['APIKEY'])
+        self.username = os.environ.get('USERNAME', creds['USERNAME'])
         self.baseurl = 'https://{username}.carto.com/'.format(username=self.username)
         self.valid_columns = set(['the_geom', 'the_geom_webmercator', 'lsad10',
                                   'name10', 'geoid10', 'affgeoid10', 'pumace10',
                                   'statefp10', 'awater10', 'aland10','updated_at',
                                   'created_at'])
+        self.auth_client = APIKeyAuthClient(base_url=self.baseurl,
+                                            api_key=self.apikey)
+        self.sql_client = SQLClient(self.auth_client)
+
+    def test_cartocontext(self):
+        """cartoframes.CartoContext properties"""
+        cc = cartoframes.CartoContext(base_url=self.baseurl,
+                                      api_key=self.apikey)
+        self.assertTrue(cc.api_key == self.apikey)
+        self.assertTrue(cc.base_url == self.baseurl)
+        self.assertTrue(cc.username == self.username)
+        self.assertTrue(not cc.is_org)
+        # TODO: how to test instances of a class?
+        # self.assertTrue(cc.auth_client.__dict__ == self.auth_client.__dict__)
+        # self.assertTrue(cc.sql_client.__dict__ == self.sql_client.__dict__)
 
     def test_cartocontext_read(self):
         """cartoframes.CartoContext.read basic usage"""
@@ -23,6 +41,7 @@ class TestCartoContext(unittest.TestCase):
         # fails on non-existent table
         with self.assertRaises(CartoException):
             df = cc.read('non_existent_table')
+
         # normal table
         df = cc.read('cb_2013_puma10_500k')
         self.assertTrue(set(df.columns) == self.valid_columns)
