14d07e1be1509981a680cb3ae51c0c825b6373cc
Andy Eschbacher <andy.eschbacher@gmail.com>
2017-07-28 15:05:17 -0400

    adds basic cartocontext tests

--- test/test_context.py
+++ test/test_context.py
@@ -2,12 +2,28 @@
 import unittest
 import os
 import cartoframes
+from carto.exceptions import CartoException
+
 
 class TestCartoContext(unittest.TestCase):
     """Tests for cartoframes.CartoContext"""
-    APIKEY = os.environ["APIKEY"]
-    USERNAME = os.environ["USERNAME"]
-    BASEURL = 'https://{username}.carto.com/'.format(username=USERNAME)
-    cc = cartoframes.CartoContext(base_url=BASEURL,
-                                  api_key=APIKEY)
-    df = cc.read('cb_2013_puma10_500k')
+    self.apikey = os.environ["APIKEY"]
+    self.username = os.environ["USERNAME"]
+    self.baseurl = 'https://{username}.carto.com/'.format(username=USERNAME)
+    self.valid_columns = set(['the_geom', 'the_geom_webmercator', 'lsad10',
+                              'name10', 'geoid10', 'affgeoid10', 'pumace10',
+                              'statefp10', 'awater10', 'aland10','updated_at',
+                              'created_at'])
+
+    def test_cartocontext_read(self):
+        """cartoframes.CartoContext.read basic usage"""
+        cc = cartoframes.CartoContext(base_url=self.baseurl,
+                                      api_key=self.apikey)
+        # fails on non-existent table
+        with self.assertRaises(CartoException):
+            df = cc.read('non_existent_table')
+        # normal table
+        df = cc.read('cb_2013_puma10_500k')
+        self.assertTrue(set(df.columns) == self.valid_columns)
+        self.assertTrue(len(df) == 2379)
+
