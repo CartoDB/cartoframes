eb99ed3230d9c8b6ff72faf03cb5ad42698df12b
Andy Eschbacher <andy.eschbacher@gmail.com>
2017-08-04 21:56:57 -0400

    more tests and some refactoring

--- test/test_maps.py
+++ test/test_maps.py
@@ -1,8 +1,8 @@
 """Unit tests for cartoframes.layers"""
 import unittest
 from cartoframes.layer import BaseMap, Layer, QueryLayer
-from cartoframes.maps import non_basemap_layers, has_time_layer, get_map_name, get_map_template
-from cartoframes.layer import BaseMap
+from cartoframes.maps import (non_basemap_layers, has_time_layer, get_map_name, 
+                              get_map_template)
 import ast
 
 
@@ -16,9 +16,7 @@ class TestMaps(unittest.TestCase):
                              color='yellow',
                              size='favoritescount')]
 
-        self.nbm_layers = non_basemap_layers(self.layers)
         self.layer_with_time = has_time_layer(self.layers)
-        self.map_name = get_map_name(self.layers, has_zoom=False)
         self.map_template = get_map_template(self.layers, has_zoom=False)
         # TODO: change this to a dict
         self.js = '{"placeholders": {"north": {"default": 45, "type": "number"}, "cartocss_1": {"default": "#layer { marker-fill: red; marker-width: 5; marker-allow-overlap: true; marker-line-color: #000; }", "type": "sql_ident"}, "cartocss_0": {"default": "#layer { marker-fill: red; marker-width: 5; marker-allow-overlap: true; marker-line-color: #000; }", "type": "sql_ident"}, "west": {"default": -45, "type": "number"}, "east": {"default": 45, "type": "number"}, "sql_0": {"default": "SELECT ST_PointFromText(\'POINT(0 0)\', 4326) as the_geom, 1 as cartodb_id, ST_PointFromText(\'Point(0 0)\', 3857) as the_geom_webmercator", "type": "sql_ident"}, "sql_1": {"default": "SELECT ST_PointFromText(\'POINT(0 0)\', 4326) as the_geom, 1 as cartodb_id, ST_PointFromText(\'Point(0 0)\', 3857) as the_geom_webmercator", "type": "sql_ident"}, "south": {"default": -45, "type": "number"}}, "version": "0.0.1", "name": "cartoframes_ver20170406_layers2_time0_baseid1_labels0_zoom0", "layergroup": {"layers": [{"type": "http", "options": {"urlTemplate": "https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png", "subdomains": "abcd"}}, {"type": "mapnik", "options": {"cartocss": "<%= cartocss_0 %>", "sql": "<%= sql_0 %>", "cartocss_version": "2.1.1"}}, {"type": "mapnik", "options": {"cartocss": "<%= cartocss_1 %>", "sql": "<%= sql_1 %>", "cartocss_version": "2.1.1"}}], "version": "1.0.1"}, "view": {"bounds": {"west": "<%= west %>", "east": "<%= east %>", "north": "<%= north %>", "south": "<%= south %>"}}}'
@@ -28,16 +26,24 @@ class TestMaps(unittest.TestCase):
 
     def test_non_basemap_layers(self):
         """maps.non_basemap_layers"""
+        nbm_layers = non_basemap_layers(self.layers)
         # ensure the layers are separated out correctly
         self.assertEqual(self.layers[1],
-                         self.nbm_layers[0])
-        self.assertEqual(self.layers[2],
-                         self.nbm_layers[1])
-        # correct number of layers passed out
-        self.assertEqual(len(self.nbm_layers), 2)
+                         nbm_layers[0],
+                         msg=('first non-basemap layer should be second layer '
+                              'in original layer list'))
 
-        # TODO: what happens if there are no data layers? E.g., only a BaseMap
-        #       layer, or even no layer at all (`[]`)?
+        self.assertEqual(self.layers[2],
+                         nbm_layers[1],
+                         msg=('second non-basemap layer should be third layer '
+                              'in original layer list'))
+
+        # correct number of layers passed out
+        self.assertEqual(len(nbm_layers), 2)
+
+        # If no data layers exist, non_basemap_layers should be an empty list
+        nbm_no_layers = non_basemap_layers([BaseMap()])
+        self.assertEqual(len(nbm_no_layers), 0)
 
     def test_has_time_layer(self):
         """maps.has_time_layers"""
@@ -45,9 +51,20 @@ class TestMaps(unittest.TestCase):
 
     def test_get_map_name(self):
         """maps.get_map_name"""
-        self.assertEqual(self.map_name,
+        map_name = get_map_name(self.layers,
+                                has_zoom=False)
+        self.assertEqual(map_name,
                          'cartoframes_ver20170406_layers2_time0_baseid1_labels0_zoom0')
-        # TODO: What are some variations on this? Maybe two more examples?
+        self.assertEqual(get_map_name(self.layers, has_zoom=True),
+                         'cartoframes_ver20170406_layers2_time0_baseid1_labels0_zoom1')
+        layers_w_time = [BaseMap('dark', labels='front'),
+                         Layer('acadia'),
+                         QueryLayer('select * from acadia limit 10',
+                                    time='foo'),
+                         Layer('biodiversity'),
+                         BaseMap('dark', labels='front', only_labels=True)]
+        self.assertEqual(get_map_name(layers_w_time, has_zoom=False),
+                         'cartoframes_ver20170406_layers3_time1_baseid1_labels1_zoom0')
 
     def test_map_template(self):
         """maps.map_template_dict"""
