dc96d15aad778ea97c3833517e2b96d3fd093e5c
Andy Eschbacher <andy.eschbacher@gmail.com>
2017-09-18 14:59:59 -0400

    adds version var to init

--- cartoframes/__init__.py
+++ cartoframes/__init__.py
@@ -2,3 +2,4 @@ from .context import CartoContext
 from .credentials import Credentials
 from .layer import BaseMap, QueryLayer, Layer
 from .styling import BinMethod
+from .__version__ import __version__
