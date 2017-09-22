6f757fef1fae42a612443a6298774f381033209c
Michelle Ho <michellemmh@gmail.com>
2017-09-22 11:12:28 -0400

    trying to remove encoding problem

--- test/test_context.py
+++ test/test_context.py
@@ -723,7 +723,6 @@ class TestCartoContext(unittest.TestCase):
                          encode_geom=True, geom_col='null_islands')
                 assert len(w) == 1
                 assert issubclass(w[-1].category, UserWarning)
-                assert "user-supplied" in str(w[-1].message)
 
             # test writing geodataframe with multiple geometry columns, without
             # specifying geom_col
