cef6e094ac2cc297ef6004aaaffb485984ab8623
Andy Eschbacher <andy.eschbacher@gmail.com>
2017-10-31 20:56:03 -0400

    adds raise not implemented error for dataframe layer

--- test/test_layer.py
+++ test/test_layer.py
@@ -1,12 +1,28 @@
 """Unit tests for cartoframes.layers"""
 import unittest
-from cartoframes.layer import BaseMap, QueryLayer, AbstractLayer
+from cartoframes.layer import BaseMap, QueryLayer, AbstractLayer, Layer
 from cartoframes import styling
+import pandas as pd
 
 
 class TestAbstractLayer(unittest.TestCase):
     def test_class(self):
         self.assertIsNone(AbstractLayer().__init__())
+
+
+class TestLayer(unittest.TestCase):
+    def setUp(self):
+        self.coffee_temps = pd.DataFrame({
+            'a': range(4),
+            'b': list('abcd')
+        })
+
+    def test_layer_setup_dataframe(self):
+        """layer.Layer._setup()"""
+        layer = Layer('cortado', source=self.coffee_temps)
+
+        with self.assertRaises(NotImplementedError):
+            layer._setup([BaseMap(), layer], 1)
 
 
 class TestBaseMap(unittest.TestCase):
