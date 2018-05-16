02c0726bdee3b0b00104703de1ca501d0755a02b
Andy Eschbacher <andyesch@protonmail.com>
2018-05-16 10:30:24 -0400

    adds docs and clearer outputs

--- cartoframes/contrib/vector.py
+++ cartoframes/contrib/vector.py
@@ -1,6 +1,7 @@
 import os
 import json
-import IPython
+from warnings import warn
+from IPython.display import HTML
 try:
     import geopandas
     HAS_GEOPANDAS = True
@@ -10,22 +11,46 @@ except ImportError:
 from .. import utils
 
 class QueryLayer:
-    def __init__(self, query, color=None, size=None, time=None):
+    """CARTO VL layer based on an arbitrary query against user database
+
+    Args:
+        query (str): Query against user database. This query must have the
+          the following columns included to successfully have a map rendered:
+          `the_geom`, `the_geom_webmercator`, and `cartodb_id`. If columns are
+          used in styling, they must be included in this query as well.
+        color (str, optional): CARTO VL color styling for this layer. Valid
+          inputs are simple web color names and hex values. For more advanced
+          styling, see the CARTO VL guide on styling for more information:
+          https://carto.com/developers/carto-vl/guides/styling-points/
+        size (int or str, optional): CARTO VL width styling for this layer if
+          points or lines (which are not yet implemented). Valid inputs are
+          positive numbers or text expressions involving variables.
+    """
+    def __init__(self, query, color=None, size=None, time=None,
+                 strokeColor=None, strokeWidth=None):
         self.query = query
         self.color = color
-        self.size = size
-        self.time = time
+        self.width = size
+        self.filter = time
+        self.strokeColor = strokeColor
+        self.strokeWidth = strokeWidth
         self.orig_query = query
         self.is_basemap = False
         self.styling = ''
 
-        # todo add torque
-        if self.color:
-            self.styling += '\ncolor: {}'.format(self.color)
-        if self.size:
-            self.styling += '\nwidth: {}'.format(self.size)
-        if self.time:
-            self.styling += '\nfilter: {}'.format(self.time)
+        self._update_style()
+
+    def _update_style(self):
+        """Appends `prop` with `style` to layer styling"""
+        valid_styles = (
+            'color', 'width', 'filter', 'strokeWidth', 'strokeColor',
+        )
+        self.styling = '\n'.join(
+            '{prop}: {style}'.format(prop=s, style=getattr(self, s))
+            for s in valid_styles
+            if getattr(self, s)
+        )
+        print(self.styling)
 
 def _get_html_doc(sources, bounds, creds=None, local_sources=None, basemap=None):
     html_template = os.path.join(
@@ -64,7 +89,7 @@ class Layer(QueryLayer):
 
 class LocalLayer(QueryLayer):
     def __init__(self, dataframe, color=None, size=None, time=None):
-        if isinstance(dataframe, geopandas.GeoDataFrame):
+        if HAS_GEOPANDAS and isinstance(dataframe, geopandas.GeoDataFrame):
             self.geojson_str = dataframe.to_json()
         else:
             raise ValueError('LocalLayer only works with GeoDataFrames')
@@ -76,7 +101,18 @@ class LocalLayer(QueryLayer):
             size=size
         )
 
-def ccmap(layers, context):
+def vmap(layers, context):
+    """CARTO VL-powered interactive map
+
+    Args:
+        layers (list of Layer-types): List of layers. One or more of
+          :obj:`Layer`, :obj:`QueryLayer`, or :obj:`LocalLayer`.
+        context (:obj:`CartoContext`): A :obj:`CartoContext` instance
+    """
+    warn(
+        'The `vector` module is in contrib, meaning that all features are '
+        'subject to change as they are experimental features'
+    )
     non_local_layers = [
         layer for layer in layers
         if not isinstance(layer, LocalLayer)
@@ -101,4 +137,4 @@ def ccmap(layers, context):
     ).format(content=utils.safe_quotes(
         _get_html_doc(jslayers, bounds, context.creds)
     ))
-    return IPython.display.HTML(html)
+    return HTML(html)
