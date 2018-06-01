3d15e89eeb8d9aa81368597335da74d806c703b6
Andy Eschbacher <andyesch@protonmail.com>
2018-06-01 15:26:03 -0400

    adds explict tests of functions

--- test/test_context.py
+++ test/test_context.py
@@ -1115,10 +1115,12 @@ class TestExamples(unittest.TestCase):
         self.examples = Examples()
 
     def test_tables(self):
+        """examples.tables"""
         tables = self.examples.tables()
         self.assertTrue(tables)
 
     def test_disabled_context_functions(self):
+        """example.Example().methods that are disabled"""
         with self.assertRaises(RuntimeError):
             self.examples.data()
         with self.assertRaises(RuntimeError):
@@ -1132,25 +1134,53 @@ class TestExamples(unittest.TestCase):
 
     def test_read_taxi(self):
         """examples.read_taxi"""
+        from cartoframes.examples import read_taxi
+        # method test
         taxi = self.examples.read_taxi()
+        self.assertIsInstance(taxi, pd.DataFrame)
+        self.assertGreater(taxi.shape[0], 0)
+
+        # function test
+        taxi = read_taxi()
         self.assertIsInstance(taxi, pd.DataFrame)
         self.assertGreater(taxi.shape[0], 0)
 
     def test_read_brooklyn_poverty(self):
         """examples.read_brooklyn_poverty"""
+        from cartoframes.examples import read_brooklyn_poverty
+        # method test
         bp = self.examples.read_brooklyn_poverty()
+        self.assertIsInstance(bp, pd.DataFrame)
+        self.assertGreaterEqual(bp.shape[0], 0)
+
+        # function test
+        bp = read_brooklyn_poverty()
         self.assertIsInstance(bp, pd.DataFrame)
         self.assertGreaterEqual(bp.shape[0], 0)
 
     def test_read_mcdonalds(self):
         """examples.read_mcdonalds"""
+        from cartoframes.examples import read_mcdonalds_nyc
+        # method test
         mcd = self.examples.read_mcdonalds_nyc()
         self.assertIsInstance(mcd, pd.DataFrame)
         self.assertGreater(mcd.shape[0], 0)
 
-    def test_read_mcdonalds(self):
-        """examples.read_mcdonalds"""
+        # function test
+        mcd = read_mcdonalds_nyc()
+        self.assertIsInstance(mcd, pd.DataFrame)
+        self.assertGreater(mcd.shape[0], 0)
+
+    def test_read_nyc_census_tracts(self):
+        """examples.read_nyc_census_tracts"""
+        from cartoframes.examples import read_nyc_census_tracts
+        # method test
         nycct = self.examples.read_nyc_census_tracts()
+        self.assertIsInstance(nycct, pd.DataFrame)
+        self.assertGreater(nycct.shape[0], 0)
+
+        # function test
+        nycct = read_nyc_census_tracts()
         self.assertIsInstance(nycct, pd.DataFrame)
         self.assertGreater(nycct.shape[0], 0)
 
