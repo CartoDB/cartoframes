5da855ed4fcb4585b4896c6203f073e245950b53
Andy Eschbacher <andyesch@protonmail.com>
2018-06-20 16:07:10 -0400

    adds basic documentation for table class

--- cartoframes/analysis.py
+++ cartoframes/analysis.py
@@ -1,4 +1,13 @@
+"""Analysis module"""
 class Table:
+    """Table class for abstract representations of tables in user's CARTO
+    account. The list of features is limited in the class now, but will be
+    expanded in the future.
+
+   Attrs:
+
+       name (str): Name of table on CARTO account
+    """
 
     def __init__(self, name):
         self.name = name
