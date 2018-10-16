594aaae0983102fcccf24df473a5655e9fc019c3
Andy Eschbacher <andyesch@protonmail.com>
2018-10-16 12:02:14 -0400

    docs on vector basemaps

--- cartoframes/contrib/vector.py
+++ cartoframes/contrib/vector.py
@@ -18,7 +18,23 @@ except ImportError:
 from .. import utils
 
 class BaseMaps:
-    """Supported CARTO vector basemaps"""
+    """Supported CARTO vector basemaps.
+
+    Attributes:
+        darkmatter (str): CARTO's "Dark Matter" style basemap
+        positron (str): CARTO's "Positron" style basemap
+        voyager (str): CARTO's "Voyager" style basemap
+
+    Example:
+        Create an embedded map using CARTO's Positron style with no data layers
+
+        .. code::
+
+            from cartoframes.contrib import vector
+            from cartoframes import CartoContext
+            cc = CartoContext()
+            vector.vmap([], context=cc, basemap=vector.BaseMaps.positron)
+    """
     positron = 'Positron'
     darkmatter = 'DarkMatter'
     voyager = 'Voyager'
