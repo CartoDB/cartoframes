540300ffdb21bdede8fbb4fe424e9a8642516385
Román Jiménez <roman@cartodb.com>
2019-04-03 14:25:32 +0200

    Point to latest Airship / VL

--- cartoframes/contrib/vector.py
+++ cartoframes/contrib/vector.py
@@ -34,7 +34,7 @@ except ImportError:
 from .. import utils
 
 # CARTO VL
-_DEFAULT_CARTO_VL_PATH = 'https://libs.cartocdn.com/carto-vl/v1.1.1/carto-vl.min.js'
+_DEFAULT_CARTO_VL_PATH = 'https://libs.cartocdn.com/carto-vl/v1.2.3/carto-vl.min.js'
 
 # AIRSHIP
 _AIRSHIP_SCRIPT = '/packages/components/dist/airship.js'
@@ -42,10 +42,10 @@ _AIRSHIP_BRIDGE_SCRIPT = '/packages/bridge/dist/asbridge.js'
 _AIRSHIP_STYLE = '/packages/styles/dist/airship.css'
 _AIRSHIP_ICONS_STYLE = '/packages/icons/dist/icons.css'
 
-_DEFAULT_AIRSHIP_COMPONENTS_PATH = 'https://libs.cartocdn.com/airship-components/v1.0.3/airship.js'
-_DEFAULT_AIRSHIP_BRIDGE_PATH = 'https://libs.cartocdn.com/airship-bridge/v1.0.3/asbridge.js'
-_DEFAULT_AIRSHIP_STYLES_PATH = 'https://libs.cartocdn.com/airship-style/v1.0.3/airship.css'
-_DEFAULT_AIRSHIP_ICONS_PATH = 'https://libs.cartocdn.com/airship-icons/v1.0.3/icons.css'
+_DEFAULT_AIRSHIP_COMPONENTS_PATH = 'https://libs.cartocdn.com/airship-components/v2/airship.js'
+_DEFAULT_AIRSHIP_BRIDGE_PATH = 'https://libs.cartocdn.com/airship-bridge/v2/asbridge.js'
+_DEFAULT_AIRSHIP_STYLES_PATH = 'https://libs.cartocdn.com/airship-style/v2/airship.css'
+_DEFAULT_AIRSHIP_ICONS_PATH = 'https://libs.cartocdn.com/airship-icons/v2/icons.css'
 
 
 class BaseMaps(object):  # pylint: disable=too-few-public-methods
