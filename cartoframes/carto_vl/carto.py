29f31ffa00ed55e1eae9c82f03e44f7697c50782
elenatorro <elenatorro@gmail.com>
2019-04-05 13:18:55 +0200

    Fix hound

--- cartoframes/carto_vl/carto.py
+++ cartoframes/carto_vl/carto.py
@@ -1,6 +1,7 @@
-from .. import utils
 from .basemap.basemap import Basemap
 from .map.map import Map
 from .layer.layer import Layer
 from .layer.local_layer import LocalLayer
 from .layer.query_layer import QueryLayer
+
+__all__ = ["Basemap", "Map", "Layer", "LocalLayer", "QueryLayer"]
