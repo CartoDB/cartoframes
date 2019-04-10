4284d21849aea1fbc6874120a629c5b09008f499
elenatorro <elenatorro@gmail.com>
2019-04-10 16:36:18 +0200

    Fix imports

--- cartoframes/carto_vl/__init__.py
+++ cartoframes/carto_vl/__init__.py
@@ -1,10 +1,10 @@
 from . import defaults
-from .utils.html import HTMLMap
-from .basemap.basemaps import Basemaps
-from .map.map import Map
-from .layer.layer import Layer
-from .layer.local_layer import LocalLayer
-from .layer.query_layer import QueryLayer
+from . utils.html import HTMLMap
+from . basemap.basemaps import Basemaps
+from . maps.map import Map
+from . layer.layer import Layer
+from . layer.local_layer import LocalLayer
+from . layer.query_layer import QueryLayer
 
 
 __all__ = ["defaults", "HTMLMap", "Basemaps", "Map", "Layer", "LocalLayer", "QueryLayer"]
