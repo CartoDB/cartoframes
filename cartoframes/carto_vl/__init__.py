64e1d6049b5eb3a28eac9d420c107c362ac4b200
elenatorro <elenatorro@gmail.com>
2019-04-10 17:14:21 +0200

    Use future import

--- cartoframes/carto_vl/__init__.py
+++ cartoframes/carto_vl/__init__.py
@@ -1,10 +1,12 @@
+from __future__ import absolute_import
+
 from . import defaults
-from . utils.html import HTMLMap
-from . basemap.basemaps import Basemaps
-from . maps.map import Map
-from . layer.layer import Layer
-from . layer.local_layer import LocalLayer
-from . layer.query_layer import QueryLayer
+from .utils.html import HTMLMap
+from .basemap.basemaps import Basemaps
+from .maps.map import Map
+from .layer.layer import Layer
+from .layer.local_layer import LocalLayer
+from .layer.query_layer import QueryLayer
 
 
 __all__ = ["defaults", "HTMLMap", "Basemaps", "Map", "Layer", "LocalLayer", "QueryLayer"]
