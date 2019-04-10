8d6e3b0e61d89cb3a7c9f49c0acc439258025884
elenatorro <elenatorro@gmail.com>
2019-04-10 15:41:10 +0200

    Rename folder

--- test/carto-vl/test_carto.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from cartoframes.carto_vl import carto
-
-
-class TestCartoVL(unittest.TestCase):
-    def test_is_defined(self):
-        self.assertNotEqual(carto, None)
--- test/carto-vl/test_map.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from cartoframes.carto_vl import carto
-
-
-class TestMap(unittest.TestCase):
-    def test_is_defined(self):
-        self.assertNotEqual(carto.Map, None)
