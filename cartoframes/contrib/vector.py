ed2131a4aeab031271dd120d97a63af2172032b2
Román Jiménez <roman@cartodb.com>
2019-04-10 12:20:43 +0200

    remove characters that python dislikes

--- cartoframes/contrib/vector.py
+++ cartoframes/contrib/vector.py
@@ -436,7 +436,7 @@ def vmap(layers,
             - zoom (float): Zoom level.
             - bearing (float): A bearing, or heading, is the direction you're facing,
                 measured clockwise as an angle from true north on a compass.
-                (north is 0°, east is 90°, south is 180°, and west is 270°).
+                (north is 0º, east is 90º, south is 180º, and west is 270º).
             - pitch (float): The angle towards the horizon measured in degrees, with a
                 range between 0 and 60 degrees. Zero degrees results in a two-dimensional
                 map, as if your line of sight forms a perpendicular angle with
