2cc4da0652d77b3141303c54c51c8d7edbc1797f
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2019-05-13 14:35:45 +0200

    Use __all__ in main __init__

--- cartoframes/__init__.py
+++ cartoframes/__init__.py
@@ -1,7 +1,18 @@
 from .context import CartoContext
 from .credentials import Credentials
-from .layer import BaseMap, QueryLayer, Layer
+from .layer import BaseMap, Layer
 from .styling import BinMethod
 from .dataset import Dataset
 from .dataset import setDefaultContext
 from .__version__ import __version__
+
+__all__ = [
+    CartoContext,
+    Credentials,
+    BaseMap,
+    Layer,
+    BinMethod,
+    Dataset,
+    setDefaultContext,
+    __version__
+]
