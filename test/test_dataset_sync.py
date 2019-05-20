0b576484cd85b322985cc508d0837e231ab37bee
Simon Martín <oleurud@gmail.com>
2019-05-20 18:19:39 +0200

    mocks in tests

--- test/test_dataset_sync.py
+++ test/test_dataset_sync.py
@@ -54,6 +54,28 @@ class TestDatasetSync(unittest.TestCase, _UserUrlLoader):
             ]
         }
 
+        def mock_download(self):
+            self.df = 4
+            return self.df
+        Dataset.download = mock_download
+
+        def mock_copyfrom(self, _):
+            return True
+        Dataset._copyfrom = mock_copyfrom
+
+        def mock_create_table(self, _):
+            return True
+        Dataset._create_table = mock_create_table
+
+        def mock_create_table_from_query(self):
+            return True
+        Dataset._create_table_from_query = mock_create_table_from_query
+
+        def mock_exists(self):
+            return False
+        Dataset.exists = mock_exists
+
+
     def test_dataset_sync_from_table(self):
         table_name = 'fake_table'
         dataset = Dataset.from_table(table_name=table_name, context=self.cc)
@@ -71,7 +93,21 @@ class TestDatasetSync(unittest.TestCase, _UserUrlLoader):
         dataset.schema = 'another_schema'
         self.assertEqual(dataset.is_sync, False)
 
-    def test_dataset_sync_from_query(self):
+    def test_dataset_sync_from_table_after_download(self):
+        table_name = 'fake_table'
+        dataset = Dataset.from_table(table_name=table_name, context=self.cc)
+        dataset.download()
+        self.assertEqual(dataset.is_sync, True)
+
+    def test_dataset_sync_from_table_after_upload(self):
+        table_name = 'fake_table'
+        dataset = Dataset.from_table(table_name=table_name, context=self.cc)
+        dataset.download()
+        dataset.upload(table_name='another_table')
+        self.assertEqual(dataset.is_sync, True)
+        self.assertEqual(dataset.table_name, 'another_table')
+
+    def test_dataset_not_sync_from_query(self):
         query = "SELECT 1"
         dataset = Dataset.from_query(query=query, context=self.cc)
         self.assertEqual(dataset.is_sync, False)
