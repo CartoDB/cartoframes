96d8082768f23301b4d82ba7ecbf3ed4a25a59e1
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2019-05-24 17:34:41 +0200

    Improve set_default_context method

--- cartoframes/auth/__init__.py
+++ cartoframes/auth/__init__.py
@@ -10,9 +10,11 @@ def set_default_context(context=None, base_url=None, api_key=None, creds=None, s
     if isinstance(context, Context):
         default_context = context
     elif isinstance(base_url, str) and isinstance(api_key, str):
-        default_context = Context(base_url, api_key, creds, session)
+        default_context = Context(base_url=base_url, api_key=api_key, session=session)
+    elif isinstance(creds, Credentials):
+        default_context = Context(creds=creds, session=session)
     else:
-        raise ValueError('Wrong context data')
+        raise ValueError('Invalid inputs. Pass a Context, a base_url and api_key pair, or a Credentials object.')
 
 
 __all__ = [
