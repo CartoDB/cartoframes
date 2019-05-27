6150e90bdcff3c7c3a5c5057caf74d4365f4366f
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2019-05-27 11:30:51 +0200

    Point to CVL develop

--- cartoframes/viz/defaults.py
+++ cartoframes/viz/defaults.py
@@ -1,4 +1,4 @@
-CARTO_VL_PATH = 'https://libs.cartocdn.com/carto-vl/v1.2/carto-vl.min.js'
+CARTO_VL_PATH = 'https://libs.cartocdn.com/carto-vl/branches/develop/carto-vl.min.js'
 
 AIRSHIP_SCRIPT = '/packages/components/dist/airship.js'
 AIRSHIP_BRIDGE_SCRIPT = '/packages/bridge/dist/asbridge.js'
