80c3f65b12e5effe0ea2d2ddba2f69bc9e39c613
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2019-06-03 15:11:14 +0200

    Refactor frontend libs version

--- cartoframes/viz/constants.py
+++ cartoframes/viz/constants.py
@@ -1,14 +1,17 @@
-CARTO_VL_DEV = '/dist/carto-vl.js'
-CARTO_VL_URL = 'https://libs.cartocdn.com/carto-vl/v1.3/carto-vl.min.js'
 
+CARTO_VL_VERSION = '1.3'
+CARTO_VL_DEV = '/dist/carto-vl.js'
+CARTO_VL_URL = 'https://libs.cartocdn.com/carto-vl/{}/carto-vl.min.js'.format(CARTO_VL_VERSION)
+
+AIRSHIP_VERSION = 'cartoframes'
 AIRSHIP_COMPONENTS_DEV = '/packages/components/dist/airship.js'
 AIRSHIP_BRIDGE_DEV = '/packages/bridge/dist/asbridge.js'
 AIRSHIP_STYLES_DEV = '/packages/styles/dist/airship.css'
 AIRSHIP_ICONS_DEV = '/packages/icons/dist/icons.css'
-AIRSHIP_COMPONENTS_URL = 'https://libs.cartocdn.com/airship-components/cartoframes/airship.js'
-AIRSHIP_BRIDGE_URL = 'https://libs.cartocdn.com/airship-bridge/cartoframes/asbridge.min.js'
-AIRSHIP_STYLES_URL = 'https://libs.cartocdn.com/airship-style/cartoframes/airship.min.css'
-AIRSHIP_ICONS_URL = 'https://libs.cartocdn.com/airship-icons/cartoframes/icons.css'
+AIRSHIP_COMPONENTS_URL = 'https://libs.cartocdn.com/airship-components/{}/airship.js'.format(AIRSHIP_VERSION)
+AIRSHIP_BRIDGE_URL = 'https://libs.cartocdn.com/airship-bridge/{}/asbridge.min.js'.format(AIRSHIP_VERSION)
+AIRSHIP_STYLES_URL = 'https://libs.cartocdn.com/airship-style/{}/airship.min.css'.format(AIRSHIP_VERSION)
+AIRSHIP_ICONS_URL = 'https://libs.cartocdn.com/airship-icons/{}/icons.css'.format(AIRSHIP_VERSION)
 
 STYLE_PROPERTIES = [
     'color',
