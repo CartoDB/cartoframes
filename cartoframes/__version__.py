0fd731a31ba864420ac3e2770d33d6b5081ef64b
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2019-06-18 16:40:47 +0200

    Bump to version 1.0b1

--- cartoframes/__version__.py
+++ cartoframes/__version__.py
@@ -1,7 +1,7 @@
 """cartoframes version number"""
 __title__ = 'cartoframes'
 __description__ = 'CARTO Python package for data scientists'
-__version__ = '1.0b1.dev'
+__version__ = '1.0b1'
 __url__ = 'https://github.com/CartoDB/cartoframes'
 __author__ = 'Andy Eschbacher'
 __email__ = 'andy@carto.com'
