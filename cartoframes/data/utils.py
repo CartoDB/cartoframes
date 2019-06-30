34d2404f6ab1189c8f0df53f4262db82b79a15a9
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2019-06-30 10:16:44 +0200

    Review support for E(WKB/WKT)

--- cartoframes/data/utils.py
+++ cartoframes/data/utils.py
@@ -43,7 +43,6 @@ ENC_SHAPELY = 'shapely'
 ENC_WKB = 'wkb'
 ENC_WKB_HEX = 'wkb-hex'
 ENC_WKB_HEX_ASCII = 'wkb-hex-ascii'
-ENC_EWKB_HEX_ASCII = 'ewkb-hex-ascii'
 ENC_WKT = 'wkt'
 ENC_EWKT = 'ewkt'
 
@@ -119,18 +118,14 @@ def _encode_decode_decorator(func):
 
 @_encode_decode_decorator
 def decode_geometry(geom, enc_type):
-    """Decode any geometry into a shapely geometry"""
-    from shapely import wkb
-    from shapely import wkt
-
+    """Decode any geometry into a shapely geometry."""
     func = {
         ENC_SHAPELY: lambda: geom,
-        ENC_WKB: lambda: wkb.loads(geom),
-        ENC_WKB_HEX: lambda: wkb.loads(ba.unhexlify(geom)),
-        ENC_WKB_HEX_ASCII: lambda: wkb.loads(geom, hex=True),
-        ENC_EWKB_HEX_ASCII: lambda: wkb.loads(_remove_srid(geom), hex=True),
-        ENC_WKT: lambda: wkt.loads(geom),
-        ENC_EWKT: lambda: wkt.loads(_remove_srid(geom))
+        ENC_WKB: lambda: _load_wkb(geom),
+        ENC_WKB_HEX: lambda: _load_wkb_hex(geom),
+        ENC_WKB_HEX_ASCII: lambda: _load_wkb_hex_ascii(geom),
+        ENC_WKT: lambda: _load_wkt(geom),
+        ENC_EWKT: lambda: _load_ewkt(geom)
     }.get(enc_type)
 
     return func() if func else geom
@@ -140,12 +135,15 @@ def detect_encoding_type(input_geom):
     """
     Detect geometry encoding type:
     - ENC_WKB: b'\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00H\x93@\x00\x00\x00\x00\x00\x9d\xb6@'
+    - ENC_EWKB: b'\x01\x01\x00\x00 \xe6\x10\x00\x00\x00\x00\x00\x00\x00H\x93@\x00\x00\x00\x00\x00\x9d\xb6@'
     - ENC_WKB_HEX: b'0101000000000000000048934000000000009db640'
+    - ENC_EWKB_HEX: b'0101000020E6100000000000000048934000000000009DB640'
     - ENC_WKB_HEX_ASCII: '0101000000000000000048934000000000009db640'
-    - ENC_EWKB_HEX_ASCII: 'SRID=4326;0101000000000000000048934000000000009db640'
+    - ENC_EWKB_HEX_ASCII: '0101000020E6100000000000000048934000000000009DB640'
     - ENC_WKT: 'POINT (1234 5789)'
     - ENC_EWKT: 'SRID=4326;POINT (1234 5789)'
     """
+    from shapely import wkb, geos
     from shapely.geometry.base import BaseGeometry
 
     if isinstance(input_geom, BaseGeometry):
@@ -159,19 +157,63 @@ def detect_encoding_type(input_geom):
             return ENC_WKB
 
     if isinstance(input_geom, str):
-        result = re.match(r'^SRID=\d+;(.*)$', input_geom)
-        prefix = 'e' if result else ''
-        geom = result.group(1) if result else input_geom
+        if re.match(r'^[0-9a-fA-F]+$', input_geom):
+            return ENC_WKB_HEX_ASCII
+        else:
+            result = re.match(r'^SRID=\d+;(.*)$', input_geom)
+            if result:
+                geom = result.group(1)
+                if geom != '':
+                    return ENC_EWKT
+            elif input_geom != '':
+                return ENC_WKT
 
-        if re.match(r'^[0-9a-fA-F]+$', geom):
-            return prefix + ENC_WKB_HEX_ASCII
-        elif geom != '':
-            return prefix + ENC_WKT
+
+def _load_wkb(geom):
+    """Load WKB or EWKB geometry."""
+    from shapely.wkb import loads
+    return loads(geom)
 
 
-def _remove_srid(text):
-    result = re.match(r'^SRID=\d+;(.*)$', text)
-    return result.group(1) if result else text
+def _load_wkb_hex(geom):
+    """Load WKB_HEX or EWKB_HEX geometry.
+    The geom must be converted to WKB/EWKB before loading.
+    """
+    from shapely.wkb import loads
+    return loads(ba.unhexlify(geom))
+
+
+def _load_wkb_hex_ascii(geom):
+    """Load WKB_HEX_ASCII or EWKB_HEX_ASCII geometry."""
+    from shapely.wkb import loads
+    return loads(geom, hex=True)
+
+
+def _load_wkt(geom):
+    """Load WKT geometry."""
+    from shapely.wkt import loads
+    return loads(geom)
+
+
+def _load_ewkt(egeom):
+    """Load EWKT geometry.
+    The SRID must be removed before loading and added after loading.
+    """
+    from shapely.wkt import loads
+    (srid, geom) = _extract_srid(egeom)
+    ogeom = loads(geom)
+    if srid:
+        from shapely.geos import lgeos
+        lgeos.GEOSSetSRID(ogeom._geom, srid)
+    return ogeom
+
+
+def _extract_srid(egeom):
+    result = re.match(r'^SRID=(\d+);(.*)$', egeom)
+    if result:
+        return (result.group(1), result.group(2))
+    else:
+        return (0, egeom)
 
 
 def recursive_read(context, query, retry_times=DEFAULT_RETRY_TIMES):
