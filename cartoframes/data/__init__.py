cae316fabb2026483bf34635dcbe2f64e0c54325
Simon Martín <oleurud@gmail.com>
2019-07-08 12:12:39 +0200

    fix module references

--- cartoframes/data/__init__.py
+++ cartoframes/data/__init__.py
@@ -1,11 +1,10 @@
 """Data namespace contains the class to manage your data: Dataset."""
 from __future__ import absolute_import
 
-from .dataset import Dataset, get_query
+from .dataset import Dataset
 from .dataset_info import DatasetInfo
 
 __all__ = [
     'Dataset',
-    'DatasetInfo',
-    'get_query'
+    'DatasetInfo'
 ]
