8cbd8ed51f54a3d8865cf1a22f1df0404650e57a
Simon Martín <oleurud@gmail.com>
2019-07-09 18:57:04 +0200

    startin dataset base

--- cartoframes/data/dataset_base.py
+++ cartoframes/data/dataset_base.py
@@ -3,8 +3,28 @@ from abc import ABCMeta, abstractmethod
 class DatasetBase():
     __metaclass__ = ABCMeta
 
+    FAIL = 'fail'
+    REPLACE = 'replace'
+    APPEND = 'append'
+
+    PRIVATE = DatasetInfo.PRIVATE
+    PUBLIC = DatasetInfo.PUBLIC
+    LINK = DatasetInfo.LINK
+
+    STATE_LOCAL = 'local'
+    STATE_REMOTE = 'remote'
+
+    GEOM_TYPE_POINT = 'point'
+    GEOM_TYPE_LINE = 'line'
+    GEOM_TYPE_POLYGON = 'polygon'
+
     def __init__(self, data):
+        from ..auth import _default_context
+        self._context = _default_context
+
         self.data = data
+
+        self._client = self._get_client()
 
     @abstractmethod
     def download(self):
