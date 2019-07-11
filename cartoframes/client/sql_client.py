9947b5852cd3328248afa7e16394d86b4c7098ba
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2019-07-11 16:36:29 +0200

    Add cartodbfy option to the create_table method

--- cartoframes/client/sql_client.py
+++ cartoframes/client/sql_client.py
@@ -11,6 +11,8 @@ class SQLClient(object):
     """
 
     def __init__(self, credentials, session=None):
+        self._is_org_user = None
+        self._creds = credentials
         self._client = create_client(credentials, session)
 
     def query(self, query, verbose=False):
@@ -57,8 +59,27 @@ class SQLClient(object):
         '''.format(query))
         return output[0].get('bounds')
 
-    def create_table(self, table_name, columns):
-        return self.execute('CREATE TABLE {0} ({1});'.format(table_name, ','.join(columns)))
+    def create_table(self, table_name, columns, cartodbfy=True):
+        is_org_user = self._check_org_user()
+        query = 'BEGIN;'
+        query += 'CREATE TABLE {0} ({1});'.format(
+            table_name,
+            ','.join(map(lambda x: ' '.join(x), columns)))
+        if cartodbfy:
+            query += 'SELECT CDB_CartoDBFyTable(\'{0}\', \'{1}\');'.format(
+                self._creds.username() if is_org_user else 'public',
+                table_name)
+        query += 'COMMIT;'
+        print(query)
+        return self.execute(query)
 
     def drop_table(self, table_name):
         return self.execute('DROP TABLE {0};'.format(table_name))
+
+    def _check_org_user(self):
+        """Report whether user is in a multiuser CARTO organization or not"""
+        if self._is_org_user is None:
+            query = 'SELECT unnest(current_schemas(\'f\'))'
+            res = self._client.execute_query(query, do_post=False)
+            self._is_org_user = res['rows'][0]['unnest'] != 'public'
+        return self._is_org_user
