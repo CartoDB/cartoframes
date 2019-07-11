0417e2bb344701c6f16a769bb46683cf44d6db15
Simon Martín <oleurud@gmail.com>
2019-07-11 17:48:07 +0200

    recovering upload/download docs comments

--- cartoframes/data/dataset.py
+++ cartoframes/data/dataset.py
@@ -55,11 +55,68 @@ class Dataset(object):
         return TableDataset(data, context, schema)
 
     def download(self, limit=None, decode_geom=False, retry_times=DOWNLOAD_RETRY_TIMES):
+        """Download / read a Dataset (table or query) from CARTO account
+        associated with the Dataset's instance of :py:class:`Context
+        <cartoframes.auth.Context>`.
+        Args:
+            limit (int, optional): The number of rows of the Dataset to
+              download. Default is to download all rows. This value must be
+              >= 0.
+            decode_geom (bool, optional): Decode Dataset geometries into
+              Shapely geometries from EWKB encoding.
+            retry_times (int, optional): Number of time to retry the download
+              in case it fails. Default is Dataset.DOWNLOAD_RETRY_TIMES.
+        Example:
+            .. code::
+                from cartoframes.data import Dataset
+                from cartoframes.auth import set_default_context
+                # use cartoframes example account
+                set_default_context('https://cartoframes.carto.com')
+                d = Dataset('brooklyn_poverty')
+                df = d.download(decode_geom=True)
+        """
         data = self._strategy.download(limit, decode_geom, retry_times)
         self._set_strategy(DataFrameDataset, data)
         return data
 
     def upload(self, with_lnglat=None, if_exists=FAIL, table_name=None, schema=None, context=None):
+        """Upload Dataset to CARTO account associated with `context`.
+        Args:
+            with_lnglat (tuple, optional): Two columns that have the longitude
+              and latitude information. If used, a point geometry will be
+              created upon upload to CARTO. Example input: `('long', 'lat')`.
+              Defaults to `None`.
+            if_exists (str, optional): Behavior for adding data from Dataset.
+              Options are 'fail', 'replace', or 'append'. Defaults to 'fail',
+              which means that the Dataset instance will not overwrite a
+              table of the same name if it exists. If the table does not exist,
+              it will be created.
+            table_name (str): Desired table name for the dataset on CARTO. If
+              name does not conform to SQL naming conventions, it will be
+              'normalized' (e.g., all lower case, adding `_` in place of spaces
+              and other special characters.
+            context (:py:class:`Context <cartoframes.auth.Context>`, optional):
+              Context of user account to send Dataset to. If not provided,
+              a default context (if set with :py:meth:`set_default_context
+              <cartoframes.auth.set_default_context>`) will attempted to be
+              used.
+        Example:
+            Send a pandas DataFrame to CARTO.
+            .. code::
+                from cartoframes.auth import set_default_context
+                from cartoframes.data import Dataset
+                import pandas as pd
+                set_default_context(
+                    base_url='https://your_user_name.carto.com',
+                    api_key='your api key'
+                )
+                df = pd.DataFrame({
+                    'lat': [40, 45, 50],
+                    'lng': [-80, -85, -90]
+                })
+                d = Dataset(df)
+                d.upload(with_lnglat=('lng', 'lat'), table_name='sample_table')
+        """
         if table_name:
             self._strategy.table_name = table_name
         if context:
