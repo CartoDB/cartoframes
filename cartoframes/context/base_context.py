addee67fbf46a795c9de4669c9951c84b6590d98
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2019-07-12 19:05:26 +0200

    Add params in BaseContext abtract methods

--- cartoframes/context/base_context.py
+++ cartoframes/context/base_context.py
@@ -5,17 +5,17 @@ class BaseContext():
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def download(self):
+    def download(self, query, retry_times=0):
         pass
 
     @abstractmethod
-    def upload(self):
+    def upload(self, query, data):
         pass
 
     @abstractmethod
-    def execute_query(self):
+    def execute_query(self, query, parse_json=True, do_post=True, format=None, **request_args):
         pass
 
     @abstractmethod
-    def execute_long_running_query(self):
+    def execute_long_running_query(self, query):
         pass
