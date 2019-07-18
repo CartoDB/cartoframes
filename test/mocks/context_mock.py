45eb2ea69e9c263b2892c197876eb6a6cbbfa0d8
Simon Martín <oleurud@gmail.com>
2019-07-18 11:21:52 +0200

    rm CredsMock

--- test/mocks/context_mock.py
+++ test/mocks/context_mock.py
@@ -1,25 +1,12 @@
 # -*- coding: utf-8 -*-
 
-
-class CredsMock():
-    def __init__(self, key=None, username=None):
-        self._key = key
-        self._username = username
-
-    def username(self):
-        return self._username
-
-    def key(self):
-        return self._key
-
-    def base_url(self):
-        return 'https://{}.carto.com/'.format(self._username)
+from cartoframes.auth import Credentials
 
 
 class ContextMock():
     def __init__(self, username, api_key):
         self.is_org = False
-        self.creds = CredsMock(key=api_key, username=username)
+        self.creds = Credentials(api_key=api_key, username=username)
         self.session = ''
 
     def get_default_schema(self):
