5ce51e079d0b91781a90539109b2c1d90616091b
elenatorro <elenatorro@gmail.com>
2019-07-22 13:26:53 +0200

    Change formula widget

--- test/viz/widgets/test_formula_widget.py
+++ test/viz/widgets/test_formula_widget.py
@@ -8,9 +8,33 @@ class TestFormulaWidget(unittest.TestCase):
         self.assertNotEqual(widgets.formula_widget, None)
 
     def test_factory(self):
-        "should create a formula widget"
+        "should create a default formula widget"
         widget = widgets.formula_widget('$value', title='Formula Widget')
         widget_info = widget.get_info()
         self.assertEqual(widget_info.get('type'), 'formula')
         self.assertEqual(widget_info.get('value'), '$value')
         self.assertEqual(widget_info.get('title'), 'Formula Widget')
+
+    def test_count_formula_viewport(self):
+        "should create a formula widget to count viewport features"
+        widget = widgets.formula_widget('count')
+        widget_info = widget.get_info()
+        self.assertEqual(widget_info.get('value'), 'viewportCount()')
+
+    def test_count_formula_global(self):
+        "should create a formula widget to count global features"
+        widget = widgets.formula_widget('count', is_global=True)
+        widget_info = widget.get_info()
+        self.assertEqual(widget_info.get('value'), 'globalCount()')
+
+    def test_formula_viewport(self):
+        "should create a formula widget to get a viewport operation"
+        widget = widgets.formula_widget('$value', 'avg')
+        widget_info = widget.get_info()
+        self.assertEqual(widget_info.get('value'), 'viewportAvg($value)')
+
+    def test_formula_global(self):
+        "should create a formula widget to get a global operation"
+        widget = widgets.formula_widget('$value', 'avg', is_global=True)
+        widget_info = widget.get_info()
+        self.assertEqual(widget_info.get('value'), 'globalAvg($value)')
