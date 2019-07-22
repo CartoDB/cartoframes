c8b6d41ee5d19a2b749b459f5f8dd4bc82b959e8
elenatorro <elenatorro@gmail.com>
2019-07-22 12:56:10 +0200

    Build default formula widget

--- cartoframes/viz/widgets/formula_widget.py
+++ cartoframes/viz/widgets/formula_widget.py
@@ -1,11 +1,14 @@
 from __future__ import absolute_import
 
 from ..widget import Widget
+from ..constants import FORMULA_OPERATIONS_VIEWPORT, FORMULA_OPERATIONS_GLOBAL
 
 
-def formula_widget(value, **kwargs):
+def formula_widget(value, operation='count', **kwargs):
     data = kwargs
     data['type'] = 'formula'
-    data['value'] = value
+    is_global = kwargs.get('global', False)
+    operation = FORMULA_OPERATIONS_GLOBAL.get(operation) if is_global else FORMULA_OPERATIONS_VIEWPORT.get(operation)
+    data['value'] = operation + '(' + value + ')'
 
     return Widget(data)
