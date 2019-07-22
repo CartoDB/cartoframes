a9b7f7d503360d0647f41b4e2fecad4d50324a49
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2019-07-22 15:51:48 +0200

    Refactor set_default_credentials method

--- cartoframes/auth/__init__.py
+++ cartoframes/auth/__init__.py
@@ -1,28 +1,29 @@
-"""Auth namespace contains the class to manage authentication: Context.
-It also includes the utility method set_default_context."""
+"""Auth namespace contains the class to manage authentication: Credentials.
+It also includes the utility method set_default_credentials."""
 from __future__ import absolute_import
 
-from .context import Context
 from .credentials import Credentials
+from ..utils import is_url
 
-_default_context = None
+_default_credentials = None
 
 
-def set_default_context(first=None, second=None, base_url=None, api_key=None, context=None, creds=None, session=None):
-    """set_default_context
+def set_default_credentials(
+    first=None, second=None, credentials=None,
+    username=None, base_url=None, api_key=None, session=None):
+    """set_default_credentials
 
     Args:
-        context (:py:class:`Context <cartoframes.auth.Context>`):
-          A :py:class:`Context <cartoframes.auth.Context>` instance.
-        base_url (str): Base URL of CARTO user account. Cloud-based accounts
+        credentials (:py:class:`Credentials <cartoframes.credentials.Credentials>`, optional):
+          A :py:class:`Credentials <cartoframes.credentials.Credentials>`
+          instance can be used in place of a `uesrname | base_url`/`api_key` combination.
+        base_url (str, optional): Base URL of CARTO user account. Cloud-based accounts
           should use the form ``https://{username}.carto.com`` (e.g.,
           https://eschbacher.carto.com for user ``eschbacher``) whether on
           a personal or multi-user account. On-premises installation users
           should ask their admin.
-        api_key (str): CARTO API key.
-        creds (:py:class:`Credentials <cartoframes.credentials.Credentials>`):
-          A :py:class:`Credentials <cartoframes.credentials.Credentials>`
-          instance can be used in place of a `base_url`/`api_key` combination.
+        api_key (str, optional): CARTO API key.
+        username (str, optional): CARTO user name of the account.
         session (requests.Session, optional): requests session. See `requests
           documentation
           <https://2.python-requests.org/en/master/user/advanced/#session-objects>`__
@@ -30,15 +31,49 @@ def set_default_context(first=None, second=None, base_url=None, api_key=None, co
 
     Example:
 
+        From a pair username, api_key
+
+        .. code::
+
+            from cartoframes.auth import set_default_credentials
+
+            set_default_credentials(
+                username='your_user_name',
+                api_key='your api key'
+            )
+
+            # or
+
+            set_default_credentials(
+                'your_user_name',
+                'your api key'
+            )
+
+        From a username (for public datasets).
+        The API key `default_public` is used by default.
+
+        .. code::
+
+            from cartoframes.auth import set_default_credentials
+
+            set_default_credentials('your_user_name')
+
         From a pair base_url, api_key.
 
         .. code::
 
-            from cartoframes.auth import set_default_context
+            from cartoframes.auth import set_default_credentials
 
-            set_default_context(
+            set_default_credentials(
                 base_url='https://your_user_name.carto.com',
                 api_key='your api key'
+            )
+
+            # or
+
+            set_default_credentials(
+                'https://your_user_name.carto.com',
+                'your api key'
             )
 
         From a base_url (for public datasets).
@@ -46,53 +81,47 @@ def set_default_context(first=None, second=None, base_url=None, api_key=None, co
 
         .. code::
 
-            from cartoframes.auth import set_default_context
+            from cartoframes.auth import set_default_credentials
 
-            set_default_context('https://your_user_name.carto.com')
+            set_default_credentials('https://your_user_name.carto.com')
 
-        From a context.
+        From a :py:class:`Credentials <cartoframes.auth.Credentials>` class.
 
         .. code::
 
-            from cartoframes.auth import Context, set_default_context
+            from cartoframes.auth import Credentials, set_default_credentials
 
-            context = Context(
+            credentials = Credentials(
                 base_url='https://your_user_name.carto.com',
                 api_key='your api key'
             )
-            set_default_context(context)
-
-        From credentials.
-
-        .. code::
-
-            from cartoframes.auth import Credentials, set_default_context
-
-            creds = Credentials(
-                api_key='abcdefg',
-                username='cartoframes'
-            )
-            set_default_context(creds)
+            set_default_credentials(credentials)
     """
-    global _default_context
-
-    _context = context if first is None else first
+    global _default_credentials
+    
     _base_url = base_url if first is None else first
+    _username = username if first is None else first
     _api_key = (api_key if second is None else second) or 'default_public'
-    _creds = creds if first is None else first
+    _credentials = credentials if first is None else first
 
-    if isinstance(_context, Context):
-        _default_context = _context
-    elif isinstance(_base_url, str) and isinstance(_api_key, str):
-        _default_context = Context(base_url=_base_url, api_key=_api_key, session=session)
-    elif isinstance(_creds, Credentials):
-        _default_context = Context(creds=_creds, session=session)
+    if isinstance(_credentials, Credentials):
+        _default_credentials = _credentials
+
+    elif isinstance(_base_url or _username, str) and isinstance(_api_key, str):
+        if is_url(_base_url):
+            _default_credentials = Credentials(base_url=_base_url, api_key=_api_key)
+        else:
+            _default_credentials = Credentials(username=_username, api_key=_api_key)
+
     else:
-        raise ValueError('Invalid inputs. Pass a Context, a base_url and api_key pair, or a Credentials object.')
+        raise ValueError(
+            'Invalid inputs. Pass a Credentials object, a username and api_key pair or a base_url and api_key pair.')
+
+    if session:
+        _default_credentials.session = session
 
 
 __all__ = [
-    'Context',
     'Credentials',
-    'set_default_context'
+    'set_default_credentials'
 ]
