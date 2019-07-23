74def524295bcc47c37529bc9f89946151b1cdfa
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2019-07-23 10:29:28 +0200

    Strip queries in APIContext

--- cartoframes/context/api_context.py
+++ cartoframes/context/api_context.py
@@ -12,11 +12,11 @@ DEFAULT_RETRY_TIMES = 3
 
 
 class APIContext(BaseContext):
-    def __init__(self, creds):
+    def __init__(self, credentials):
         self.auth_client = APIKeyAuthClient(
-            base_url=creds.base_url,
-            api_key=creds.api_key,
-            session=creds.session,
+            base_url=credentials.base_url,
+            api_key=credentials.api_key,
+            session=credentials.session,
             client_id='cartoframes_{}'.format(__version__),
             user_agent='cartoframes_{}'.format(__version__)
         )
@@ -26,24 +26,24 @@ class APIContext(BaseContext):
 
     def download(self, query, retry_times=DEFAULT_RETRY_TIMES):
         try:
-            return self.copy_client.copyto_stream(query)
+            return self.copy_client.copyto_stream(query.strip())
         except CartoRateLimitException as err:
             if retry_times > 0:
                 retry_times -= 1
                 warn('Read call rate limited. Waiting {s} seconds'.format(s=err.retry_after))
                 time.sleep(err.retry_after)
                 warn('Retrying...')
-                return self.download(query, retry_times=retry_times)
+                return self.download(query.strip(), retry_times=retry_times)
             else:
                 warn(('Read call was rate-limited. '
                       'This usually happens when there are multiple queries being read at the same time.'))
                 raise err
 
     def upload(self, query, data):
-        return self.copy_client.copyfrom(query, data)
+        return self.copy_client.copyfrom(query.strip(), data)
 
     def execute_query(self, query, parse_json=True, do_post=True, format=None, **request_args):
-        return self.sql_client.send(query, parse_json, do_post, format, **request_args)
+        return self.sql_client.send(query.strip(), parse_json, do_post, format, **request_args)
 
     def execute_long_running_query(self, query):
-        return self.batch_sql_client.create_and_wait_for_completion(query)
+        return self.batch_sql_client.create_and_wait_for_completion(query.strip())
