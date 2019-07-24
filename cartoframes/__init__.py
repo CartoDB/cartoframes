48d96eb229099690de1768720b9c973c5325dcaa
Simon Martín <oleurud@gmail.com>
2019-07-24 15:47:34 +0200

    rm layer maps and styling

--- cartoframes/__init__.py
+++ cartoframes/__init__.py
@@ -1,12 +1,6 @@
-from .layer import BaseMap, QueryLayer, Layer
-from .styling import BinMethod
 from .__version__ import __version__
 
 
 __all__ = [
     '__version__',
-    'BaseMap',
-    'QueryLayer',
-    'Layer',
-    'BinMethod'
 ]
