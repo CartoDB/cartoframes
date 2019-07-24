a4e8ad3f7bee310b6958bda4300e154181483f67
Simon Martín <oleurud@gmail.com>
2019-07-24 16:15:56 +0200

    tables feature from context

--- cartoframes/data/__init__.py
+++ cartoframes/data/__init__.py
@@ -4,9 +4,12 @@ from __future__ import absolute_import
 from .dataset import Dataset
 from .dataset_info import DatasetInfo
 from .registry.strategies_registry import StrategiesRegistry
+from .table import tables
+
 
 __all__ = [
     'Dataset',
     'DatasetInfo',
-    'StrategiesRegistry'
+    'StrategiesRegistry',
+    'tables'
 ]
