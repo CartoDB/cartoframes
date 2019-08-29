87fa9ad56b2a94750ffc1ae55627124d9d3a8358
makella <makella@cartodb.com>
2019-08-29 14:58:53 -0600

    expose additional parameters for size bins layer

--- cartoframes/viz/helpers/size_bins_layer.py
+++ cartoframes/viz/helpers/size_bins_layer.py
@@ -2,11 +2,13 @@ from __future__ import absolute_import
 
 from ..layer import Layer
 
+from .. import defaults
 
 def size_bins_layer(
         source, value, title='', method='quantiles', bins=5,
-        breaks=None, size=None, color=None, description='', footer='',
-        legend=True, popup=True, widget=False, animate=None):
+        breaks=None, size=None, color=None, opacity=None, strokewidth=None, 
+        strokecolor=None, description='', footer='',legend=True, 
+        popup=True, widget=False, animate=None):
     """Helper function for quickly creating a size symbol map with
     classification method/buckets.
 
@@ -24,7 +26,13 @@ def size_bins_layer(
         color (str, optional): Hex value, rgb expression, or other valid
           CARTO VL color. Default is '#EE5D5A' for point geometries and
           '#4CC8A3' for lines.
+        opacity (str, optional): Opacity value for point color and line features. 
+            Default is '0.8'.
+        strokewidth (str, optional): Size of the stroke on point features.
+        strokecolor (str, optional): Color of the stroke on point features.
+            Default is '#222'.
         description (str, optional): Description text legend placed under legend title.
+        footer (str, optional): Footer text placed under legend items.
         legend (bool, optional): Display map legend: "True" or "False".
           Set to "True" by default.
         popup (bool, optional): Display popups on hover and click: "True" or "False".
@@ -54,15 +62,19 @@ def size_bins_layer(
             'point': {
                 'width': 'ramp({0}(${1}, {2}), {3})'.format(
                     func, value, breaks or bins, size or [2, 14]),
-                'color': 'opacity({0}, 0.8)'.format(
-                    color or '#EE4D5A'),
+                'color': 'opacity({0}, {1})'.format(
+                    color or '#EE4D5A', opacity or '0.8'),
+                'strokeWidth': '{0}'.format(
+                    strokewidth or defaults.STYLE['point']['strokeWidth']),
+                'strokeColor': '{0}'.format(
+                    strokecolor or defaults.STYLE['point']['strokeColor']),
                 'filter': animation_filter
             },
             'line': {
                 'width': 'ramp({0}(${1}, {2}), {3})'.format(
                     func, value, breaks or bins, size or [1, 10]),
-                'color': 'opacity({0}, 0.8)'.format(
-                    color or '#4CC8A3'),
+                'color': 'opacity({0}, {1})'.format(
+                    color or '#4CC8A3', opacity or '0.8'),
                 'filter': animation_filter
             }
         },
