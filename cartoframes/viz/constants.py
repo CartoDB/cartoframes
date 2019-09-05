2edfb3e85b19b0fe2dafc8bb348d15ca557515fe
elenatorro <elenatorro@gmail.com>
2019-09-05 18:50:04 +0200

    Use cartoframes airship CDN for testing

--- cartoframes/viz/constants.py
+++ cartoframes/viz/constants.py
@@ -2,14 +2,15 @@ CARTO_VL_VERSION = 'v1.4'
 CARTO_VL_DEV = '/dist/carto-vl.js'
 CARTO_VL_URL = 'https://libs.cartocdn.com/carto-vl/{}/carto-vl.min.js'.format(CARTO_VL_VERSION)
 
-AIRSHIP_VERSION = 'v2.2.0-rc.2'
+# AIRSHIP_VERSION = 'v2.2.0-rc.2'
+AIRSHIP_VERSION = 'vcartoframes'
 AIRSHIP_COMPONENTS_DEV = '/packages/components/dist/airship.js'
 AIRSHIP_BRIDGE_DEV = '/packages/bridge/dist/asbridge.js'
 AIRSHIP_MODULE_DEV = '/packages/components/dist/airship/airship.esm.js'
 AIRSHIP_STYLES_DEV = '/packages/styles/dist/airship.css'
 AIRSHIP_ICONS_DEV = '/packages/icons/dist/icons.css'
 AIRSHIP_COMPONENTS_URL = 'https://libs.cartocdn.com/airship-components/{}/airship.js'.format(AIRSHIP_VERSION)
-AIRSHIP_BRIDGE_URL = 'https://libs.cartocdn.com/airship-bridge/{}/asbridge.min.js'.format(AIRSHIP_VERSION)
+AIRSHIP_BRIDGE_URL = 'https://libs.cartocdn.com/airship-bridge/{}/asbridge.js'.format(AIRSHIP_VERSION)
 AIRSHIP_MODULE_URL = 'https://libs.cartocdn.com/airship-components/{}/airship/airship.esm.js'.format(AIRSHIP_VERSION)
 AIRSHIP_STYLES_URL = 'https://libs.cartocdn.com/airship-style/{}/airship.min.css'.format(AIRSHIP_VERSION)
 AIRSHIP_ICONS_URL = 'https://libs.cartocdn.com/airship-icons/{}/icons.css'.format(AIRSHIP_VERSION)
