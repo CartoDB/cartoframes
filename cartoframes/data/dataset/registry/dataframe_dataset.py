5ea0b24434a5398861ae6f7835b72ba4c1c462c5
Simon Martín <oleurud@gmail.com>
2019-09-16 19:24:30 +0200

    fix enc type

--- cartoframes/data/dataset/registry/dataframe_dataset.py
+++ cartoframes/data/dataset/registry/dataframe_dataset.py
@@ -53,14 +53,14 @@ class DataFrameDataset(BaseDataset):
     def upload(self, if_exists, with_lnglat):
         self._is_ready_for_upload_validation()
 
-        columns, geom_column, geometry = _process_columns(self._df, with_lnglat)
+        columns, geom_column, enc_type = _process_columns(self._df, with_lnglat)
 
         if if_exists == BaseDataset.REPLACE or not self.exists():
             self._create_table(columns)
         elif if_exists == BaseDataset.FAIL:
             raise self._already_exists_error()
 
-        self._copyfrom(columns, geom_column, geometry, with_lnglat)
+        self._copyfrom(columns, geom_column, enc_type, with_lnglat)
 
     def delete(self):
         raise ValueError('Method not allowed in DataFrameDataset. You should use a TableDataset: `Dataset(my_table)`')
@@ -69,12 +69,12 @@ class DataFrameDataset(BaseDataset):
         """Compute the geometry type from the data"""
         return self._get_geom_type()
 
-    def _copyfrom(self, columns, geom_column, geometry, with_lnglat):
+    def _copyfrom(self, columns, geom_column, enc_type, with_lnglat):
         query = """COPY {table_name}({columns}) FROM stdin WITH (FORMAT csv, DELIMITER '|');""".format(
             table_name=self._table_name,
             columns=','.join(c['database'] for c in columns))
 
-        data = _rows(self._df, columns, geom_column, geometry, with_lnglat)
+        data = _rows(self._df, columns, geom_column, enc_type, with_lnglat)
 
         self._context.upload(query, data)
 
@@ -122,7 +122,7 @@ class DataFrameDataset(BaseDataset):
                 return map_geom_type(geometry.geom_type)
 
 
-def _rows(df, columns, geom_column, geometry, with_lnglat):
+def _rows(df, columns, geom_column, enc_type, with_lnglat):
     for i, row in df.iterrows():
         row_data = []
         the_geom_val = None
@@ -144,7 +144,9 @@ def _rows(df, columns, geom_column, geometry, with_lnglat):
                 row_data.append('{}'.format(val))
 
         if the_geom_val:
-            row_data.append('SRID=4326;{}'.format(geometry.wkt))
+            geom = decode_geometry(the_geom_val, enc_type)
+            if geom:
+                row_data.append('SRID=4326;{}'.format(geom.wkt))
         elif with_lnglat and lng_val and lat_val:
             row_data.append('SRID=4326;POINT({lng} {lat})'.format(lng=lng_val, lat=lat_val))
 
@@ -167,12 +169,12 @@ def _is_null(val):
 
 def _process_columns(df, with_lnglat=None):
     geom_column = _get_geom_col_name(df)
-    geometry = _get_geometry(df, geom_column)
+    geom_type, enc_type = _get_geometry_type(df, geom_column)
 
     columns = [{
         'dataframe': c,
         'database': _database_column_name(c, geom_column),
-        'database_type': _db_column_type(df, c, geom_column, geometry)
+        'database_type': _db_column_type(df, c, geom_column, geom_type)
     } for c in df.columns]
 
     if geom_column is None and with_lnglat:
@@ -182,7 +184,7 @@ def _process_columns(df, with_lnglat=None):
             'database_type': 'geometry(Point, 4326)'
         })
 
-    return columns, geom_column, geometry
+    return columns, geom_column, enc_type
 
 
 def _database_column_name(column, geom_column):
@@ -196,9 +198,9 @@ def _database_column_name(column, geom_column):
     return normalized_name
 
 
-def _db_column_type(df, column, geom_column, geometry):  # TODO: detect geometries
+def _db_column_type(df, column, geom_column, geom_type):  # TODO: detect geometries
     if geom_column and column == geom_column:
-        db_type = 'geometry({}, 4326)'.format(geometry.geom_type)
+        db_type = 'geometry({}, 4326)'.format(geom_type)
     else:
         db_type = _dtypes2pg(df.dtypes[column])
 
@@ -239,12 +241,13 @@ def _detect_geometry_encoding_type(df, geom_col):
     return ''
 
 
-def _get_geometry(df, geom_col):
+def _get_geometry_type(df, geom_col):
     if geom_col is not None:
         first_geom = _first_value(df[geom_col])
         if first_geom:
             enc_type = detect_encoding_type(first_geom)
-            return decode_geometry(first_geom, enc_type)
+            geom = decode_geometry(first_geom, enc_type)
+            return geom.geom_type, enc_type
 
 
 def _first_value(series):
