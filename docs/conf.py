b16dfe92b20ed311e34a17f1db0733b1022e5f60
elenatorro <elenatorro@gmail.com>
2019-09-17 15:09:28 +0200

    Exlude tests

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-
 # -- Project information -----------------------------------------------------
 
 project = u'cartoframes'
@@ -40,7 +39,6 @@ release = u''
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
     'sphinx.ext.viewcode',
 ]
 
@@ -66,7 +64,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = [u'build', 'Thumbs.db', '.DS_Store', 'test', 'setup.py']
+exclude_patterns = [u'build', 'Thumbs.db', '.DS_Store', 'test']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
@@ -88,7 +86,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
