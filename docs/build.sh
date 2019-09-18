e36b0827a03124762e5a258e5fe59ca3bcf878a7
elenatorro <elenatorro@gmail.com>
2019-09-18 12:09:26 +0200

    Change build script

--- docs/build.sh
+++ docs/build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+rm setup.rst
+rm modules.rst
+rm cartoframes*
+rm test*
+
 cd ..
 pip uninstall cartoframes -y
 pip install .
+sphinx-apidoc -o docs .
 
 cd docs
-sphinx-apidoc -o . ..
 make clean && make html && make json
