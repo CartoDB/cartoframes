50a2a99a367f589cb0f0fc8ac014c8b6fb31978b
Simon Martín <oleurud@gmail.com>
2019-09-20 11:10:45 +0200

    moving and refactoring Column classes tests

--- test/utils/test_columns.py
+++ test/utils/test_columns.py
@@ -2,8 +2,9 @@
 
 """Unit tests for cartoframes.data.columns"""
 import unittest
+import pandas as pd
 
-from cartoframes.utils.columns import Column, normalize_names, pg2dtypes
+from cartoframes.utils.columns import Column, normalize_names, pg2dtypes, DataframeColumnInfo, DataframeColumnsInfo
 
 
 class TestColumns(unittest.TestCase):
@@ -82,3 +83,158 @@ class TestColumns(unittest.TestCase):
         for i in results:
             result = pg2dtypes(i)
             self.assertEqual(result, results[i])
+
+    def test_database_column_name_the_geom(self):
+        geom_column = 'the_geom'
+
+        dataframe_column_info = DataframeColumnInfo('other', geom_column)
+        self.assertEqual(dataframe_column_info.database, 'other')
+        dataframe_column_info = DataframeColumnInfo('the_geom', geom_column)
+        self.assertEqual(dataframe_column_info.database, 'the_geom')
+
+        geom_column = 'other_geom'
+
+        dataframe_column_info = DataframeColumnInfo('other', geom_column)
+        self.assertEqual(dataframe_column_info.database, 'other')
+        dataframe_column_info = DataframeColumnInfo('the_geom', geom_column)
+        self.assertEqual(dataframe_column_info.database, 'the_geom')
+
+    def test_column_info_with_geom(self):
+        df = pd.DataFrame(
+            [['Gran Vía 46', 'Madrid', 'POINT (0 0)'], ['Ebro 1', 'Sevilla', 'POINT (1 1)']],
+            columns=['address', 'city', 'geometry'])
+
+        expected_columns = [
+            {
+                'dataframe': 'address',
+                'database': 'address',
+                'database_type': 'text'
+            },
+            {
+                'dataframe': 'city',
+                'database': 'city',
+                'database_type': 'text'
+            },
+            {
+                'dataframe': 'geometry',
+                'database': 'the_geom',
+                'database_type': 'geometry(Point, 4326)'
+            }
+        ]
+        expected_geom_column = 'geometry'
+        expected_enc_type = 'wkt'
+
+        dataframe_columns_info = DataframeColumnsInfo(df, None)
+
+        self.assertEqual(expected_columns, dataframe_columns_info.columns)
+        self.assertEqual(expected_geom_column, dataframe_columns_info.geom_column)
+        self.assertEqual(expected_enc_type, dataframe_columns_info.enc_type)
+
+    def test_column_info_with_lnglat(self):
+        df = pd.DataFrame([['0', '1'], ['0', '1']], columns=['lng', 'lat'])
+
+        expected_columns = [
+            {
+                'dataframe': 'lng',
+                'database': 'lng',
+                'database_type': 'text'
+            },
+            {
+                'dataframe': 'lat',
+                'database': 'lat',
+                'database_type': 'text'
+            },
+            {
+                'dataframe': None,
+                'database': 'the_geom',
+                'database_type': 'geometry(Point, 4326)'
+            }
+        ]
+        expected_geom_column = None
+        expected_enc_type = None
+
+        dataframe_columns_info = DataframeColumnsInfo(df, ('lng', 'lat'))
+
+        self.assertEqual(expected_columns, dataframe_columns_info.columns)
+        self.assertEqual(expected_geom_column, dataframe_columns_info.geom_column)
+        self.assertEqual(expected_enc_type, dataframe_columns_info.enc_type)
+
+    def test_column_info_without_geom(self):
+        df = pd.DataFrame(
+            [['Gran Vía 46', 'Madrid'], ['Ebro 1', 'Sevilla']], columns=['address', 'city'])
+
+        expected_columns = [
+            {
+                'dataframe': 'address',
+                'database': 'address',
+                'database_type': 'text'
+            },
+            {
+                'dataframe': 'city',
+                'database': 'city',
+                'database_type': 'text'
+            }
+        ]
+        expected_geom_column = None
+        expected_enc_type = None
+
+        dataframe_columns_info = DataframeColumnsInfo(df, None)
+
+        self.assertEqual(expected_columns, dataframe_columns_info.columns)
+        self.assertEqual(expected_geom_column, dataframe_columns_info.geom_column)
+        self.assertEqual(expected_enc_type, dataframe_columns_info.enc_type)
+
+    def test_column_info_basic_troubled_names(self):
+        df = pd.DataFrame(
+            [[1, 'POINT (1 1)', 'fake_geom']], columns=['cartodb_id', 'the_geom', 'the_geom_webmercator'])
+
+        expected_columns = [
+            {
+                'dataframe': 'cartodb_id',
+                'database': 'cartodb_id',
+                'database_type': 'bigint'
+            },
+            {
+                'dataframe': 'the_geom',
+                'database': 'the_geom',
+                'database_type': 'geometry(Point, 4326)'
+            }
+        ]
+        expected_geom_column = 'the_geom'
+        expected_enc_type = 'wkt'
+
+        dataframe_columns_info = DataframeColumnsInfo(df, None)
+
+        self.assertEqual(expected_columns, dataframe_columns_info.columns)
+        self.assertEqual(expected_geom_column, dataframe_columns_info.geom_column)
+        self.assertEqual(expected_enc_type, dataframe_columns_info.enc_type)
+
+    def test_column_info_geometry_troubled_names(self):
+        df = pd.DataFrame(
+            [['POINT (0 0)', 'POINT (1 1)', 'POINT (2 2)']], columns=['geom', 'the_geom', 'geometry'])
+
+        expected_columns = [
+            {
+                'dataframe': 'geom',
+                'database': 'geom',
+                'database_type': 'text'
+            },
+            {
+                'dataframe': 'the_geom',
+                'database': 'the_geom',
+                'database_type': 'geometry(Point, 4326)'
+            },
+            {
+                'dataframe': 'geometry',
+                'database': 'geometry',
+                'database_type': 'text'
+            },
+        ]
+        expected_geom_column = 'the_geom'
+        expected_enc_type = 'wkt'
+
+        dataframe_columns_info = DataframeColumnsInfo(df, None)
+
+        self.assertEqual(expected_columns, dataframe_columns_info.columns)
+        self.assertEqual(expected_geom_column, dataframe_columns_info.geom_column)
+        self.assertEqual(expected_enc_type, dataframe_columns_info.enc_type)
