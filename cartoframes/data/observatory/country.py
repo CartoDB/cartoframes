ac9dca32e5975e4314f17990668334a765826d20
Esther Lozano <esloho@gmail.com>
2019-09-25 16:14:04 +0200

    Small improvement to reduce number of methods

--- cartoframes/data/observatory/country.py
+++ cartoframes/data/observatory/country.py
@@ -8,6 +8,9 @@ _COUNTRY_ID_FIELD = 'country_iso_code3'
 
 class Country(SingleEntity):
 
+    id_field = _COUNTRY_ID_FIELD
+    entity_repo = get_country_repo()
+
     def datasets(self):
         return get_dataset_repo().get_by_country(self._get_id())
 
@@ -15,40 +18,19 @@ class Country(SingleEntity):
         return get_geography_repo().get_by_country(self._get_id())
 
     @classmethod
-    def _get_id_field(cls):
-        return _COUNTRY_ID_FIELD
-
-    @classmethod
-    def _get_single_entity_class(cls):
-        return Country
-
-    @classmethod
     def _get_entities_list_class(cls):
         return Countries
-
-    @classmethod
-    def _get_entity_repo(cls):
-        return get_country_repo()
 
 
 class Countries(EntitiesList):
 
+    id_field = _COUNTRY_ID_FIELD
+    entity_repo = get_country_repo()
+
     def __init__(self, data):
         super(Countries, self).__init__(data)
-        self._set_index()
-
-    @classmethod
-    def _get_id_field(cls):
-        return _COUNTRY_ID_FIELD
+        self._use_id_as_index()
 
     @classmethod
     def _get_single_entity_class(cls):
         return Country
-
-    @classmethod
-    def _get_entities_list_class(cls):
-        return Countries
-
-    @classmethod
-    def _get_entity_repo(cls):
-        return get_country_repo()
--- cartoframes/data/observatory/entity.py
+++ cartoframes/data/observatory/entity.py
@@ -1,34 +1,25 @@
 import pandas as pd
 
-from abc import ABC, abstractmethod
+from abc import ABC
 from cartoframes.exceptions import DiscoveryException
 
 
 class CatalogEntity(ABC):
 
-    @classmethod
-    @abstractmethod
-    def _get_id_field(cls):
-        raise NotImplementedError
+    id_field = None
+    entity_repo = None
 
     @classmethod
-    @abstractmethod
-    def _get_entity_repo(cls):
-        raise NotImplementedError
-
-    @classmethod
-    @abstractmethod
     def _get_single_entity_class(cls):
-        raise NotImplementedError
+        return cls
 
     @classmethod
-    @abstractmethod
     def _get_entities_list_class(cls):
-        raise NotImplementedError
+        return cls
 
     @classmethod
     def get_by_id(cls, id_):
-        return cls._get_entity_repo().get_by_id(id_)
+        return cls.entity_repo.get_by_id(id_)
 
     def __eq__(self, other):
         return self.equals(other)
@@ -45,21 +36,18 @@ class SingleEntity(CatalogEntity, pd.Series, ABC):
 
     def _get_id(self):
         try:
-            return self[self._get_id_field()]
+            return self[self.id_field]
         except KeyError:
             raise DiscoveryException('Unsupported function: this instance actually represents a subset of entities.'
                                      ' You should use the method `get_by_id("id")` to obtain a valid '
                                      'instance of the class and then attempt this function on it.')
-
-    def __eq__(self, other):
-        return self.equals(other)
 
 
 class EntitiesList(CatalogEntity, pd.DataFrame, ABC):
 
     @property
     def _constructor(self):
-        return self._get_entities_list_class()
+        return self.__class__
 
     @property
     def _constructor_sliced(self):
@@ -67,11 +55,11 @@ class EntitiesList(CatalogEntity, pd.DataFrame, ABC):
 
     @property
     def _constructor_expanddim(self):
-        return self._get_entities_list_class()
+        return self.__class__
 
     @classmethod
     def get_all(cls):
-        return cls._get_entity_repo().get_all()
+        return cls.entity_repo.get_all()
 
-    def _set_index(self):
-        self.set_index(self._get_id_field(), inplace=True, drop=False)
+    def _use_id_as_index(self):
+        self.set_index(self.id_field, inplace=True, drop=False)
