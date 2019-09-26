e2207d8d3ef033da24e42de44928d9a05d871784
Simon Martín <oleurud@gmail.com>
2019-09-26 16:25:17 +0200

    test refresh token exception

--- test/data/client/test_bigquery_client.py
+++ test/data/client/test_bigquery_client.py
@@ -2,8 +2,13 @@ import unittest
 import os
 import json
 
+from google.auth.exceptions import RefreshError
+
+from carto.exceptions import CartoException
+
 from cartoframes.auth import Credentials
 from cartoframes.data.clients.bigquery_client import BigQueryClient
+from google.cloud import bigquery
 
 _WORKING_PROJECT = 'carto-do-customers'
 
@@ -23,3 +28,15 @@ class TestBigQueryClient(unittest.TestCase):
     def test_instantiation(self):
         bq_client = BigQueryClient(_WORKING_PROJECT, self.credentials)
         self.assertIsInstance(bq_client, BigQueryClient)
+
+    def test_refresh_token_raises_cartoexception(self):
+        def query_raiser(self, query, **kwargs):
+            raise RefreshError()
+        original_query_method = BigQueryClient.query
+        bigquery.Client.query = query_raiser
+
+        bq_client = BigQueryClient(_WORKING_PROJECT, self.credentials)
+        with self.assertRaises(CartoException):
+            bq_client.query('select * from')
+
+        bigquery.Client.query = original_query_method
