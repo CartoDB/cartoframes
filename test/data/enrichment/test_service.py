ada71c3b3d503f4723b67f800d77e5849fbd0194
Simon Martín <oleurud@gmail.com>
2019-09-27 18:40:19 +0200

    test definition and leaving

---
+++ test/data/enrichment/test_service.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class TestEnrichmentService(unittest.TestCase):
+    def test_prepare_data(self):
+        pass
+
+    def test_upload_dataframe(self):
+        pass
+
+    def test_enrichment_query(self):
+        pass
+
+    def test_execute_enrichment(self):
+        pass
