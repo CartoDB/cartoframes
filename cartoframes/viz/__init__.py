89d49bfa3700f4123159a79bbab04e76c72ba292
elenatorro <elenatorro@gmail.com>
2019-10-03 13:54:43 +0200

    Fix Basemaps and Themes docs

--- cartoframes/viz/__init__.py
+++ cartoframes/viz/__init__.py
@@ -2,7 +2,9 @@
 Map and Layer. It also includes our basemaps and the helper methods."""
 from __future__ import absolute_import
 
+from .basemaps import Basemaps
 from .basemaps import Basemaps as basemaps
+from .themes import Themes
 from .themes import Themes as themes
 from .map import Map
 from .layout import Layout
@@ -17,7 +19,9 @@ from .widget_list import WidgetList
 
 __all__ = [
     'basemaps',
+    'Basemaps',
     'themes',
+    'Themes',
     'Map',
     'Layout',
     'Layer',
--- cartoframes/viz/basemaps.py
+++ cartoframes/viz/basemaps.py
@@ -2,11 +2,6 @@ class Basemaps(object):  # pylint: disable=too-few-public-methods
     """Supported CARTO basemaps. Read more about the styles in the
     `CARTO Basemaps repository <https://github.com/CartoDB/basemap-styles>`__.
 
-    Args:
-        darkmatter (str): CARTO's "Dark Matter" style basemap
-        positron (str): CARTO's "Positron" style basemap
-        voyager (str): CARTO's "Voyager" style basemap
-
     Example:
 
         Create an embedded map using CARTO's Positron style with no data layers
