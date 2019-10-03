5cb37915ceffc7fbfce30c064742c8ac3dff5061
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2019-10-03 15:50:42 +0200

    Update layout tests

--- tests/unit/viz/test_layout.py
+++ tests/unit/viz/test_layout.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 
 from carto.exceptions import CartoException
 from cartoframes.viz import Map, Layout, Layer, Source
@@ -8,45 +8,46 @@ from .utils import build_geojson
 SOURCE = build_geojson([-10, 0], [-10, 0])
 
 
-class TestLayout(unittest.TestCase):
+class TestLayout(object):
     def test_is_defined(self):
         """Layout"""
-        self.assertNotEqual(Layout, None)
+        assert Layout is not None
 
 
-class TestLayoutInitialization(unittest.TestCase):
+class TestLayoutInitialization(object):
     def test__init(self):
         """Layout should init properly"""
         layout = Layout([])
-        self.assertIsNotNone(layout._layout)
-        self.assertEqual(layout._n_size, 0)
-        self.assertEqual(layout._m_size, 1)
-        self.assertIsNone(layout._viewport)
-        self.assertTrue(layout._is_static)
+        assert layout._layout is not None
+        assert layout._n_size == 0
+        assert layout._m_size == 1
+        assert layout._viewport is None
+        assert layout._is_static is True
 
     def test__init_maps(self):
         """Layout should init properly"""
         layout = Layout([Map(Layer(Source(SOURCE))),
                          Map(Layer(Source(SOURCE)))])
-        self.assertEqual(layout._n_size, 2)
-        self.assertEqual(layout._m_size, 1)
+        assert layout._n_size == 2
+        assert layout._m_size == 1
 
     def test__init_maps_valid(self):
         """Layout should raise an error if any element in the map list is not a Map"""
 
         msg = 'All the elements in the Layout should be an instance of Map'
-        with self.assertRaises(CartoException, msg=msg):
+        with pytest.raises(CartoException) as e:
             Layout([Layer(Source(SOURCE))])
+            assert str(e) == msg
 
     def test__init_maps_size(self):
         """Layout should init properly"""
         layout = Layout([Map(Layer(Source(SOURCE))),
                          Map(Layer(Source(SOURCE)))], 1, 2)
-        self.assertEqual(layout._n_size, 1)
-        self.assertEqual(layout._m_size, 2)
+        assert layout._n_size == 1
+        assert layout._m_size == 2
 
 
-class TestLayoutSettings(unittest.TestCase):
+class TestLayoutSettings(object):
     def test_global_viewport(self):
         """Layout should return the same viewport for every map"""
         layout = Layout([
@@ -54,8 +55,8 @@ class TestLayoutSettings(unittest.TestCase):
             Map(Layer(Source(SOURCE)))
         ], viewport={'zoom': 5})
 
-        self.assertEqual(layout._layout[0].get('viewport'), {'zoom': 5})
-        self.assertEqual(layout._layout[1].get('viewport'), {'zoom': 5})
+        assert layout._layout[0].get('viewport') == {'zoom': 5}
+        assert layout._layout[1].get('viewport') == {'zoom': 5}
 
     def test_custom_viewport(self):
         """Layout should return a different viewport for every map"""
@@ -64,8 +65,8 @@ class TestLayoutSettings(unittest.TestCase):
             Map(Layer(Source(SOURCE)))
         ], viewport={'zoom': 5})
 
-        self.assertEqual(layout._layout[0].get('viewport'), {'zoom': 2})
-        self.assertEqual(layout._layout[1].get('viewport'), {'zoom': 5})
+        assert layout._layout[0].get('viewport') == {'zoom': 2}
+        assert layout._layout[1].get('viewport') == {'zoom': 5}
 
     def test_global_camera(self):
         """Layout should return the correct camera for each map"""
@@ -74,10 +75,10 @@ class TestLayoutSettings(unittest.TestCase):
             Map(Layer(Source(SOURCE)))
         ], viewport={'zoom': 5})
 
-        self.assertEqual(layout._layout[0].get('camera'),
-                         {'bearing': None, 'center': None, 'pitch': None, 'zoom': 5})
-        self.assertEqual(layout._layout[1].get('camera'),
-                         {'bearing': None, 'center': None, 'pitch': None, 'zoom': 5})
+        assert layout._layout[0].get('camera') == {
+            'bearing': None, 'center': None, 'pitch': None, 'zoom': 5}
+        assert layout._layout[1].get('camera') == {
+            'bearing': None, 'center': None, 'pitch': None, 'zoom': 5}
 
     def test_custom_camera(self):
         """Layout should return the correct camera for each map"""
@@ -86,10 +87,10 @@ class TestLayoutSettings(unittest.TestCase):
             Map(Layer(Source(SOURCE)))
         ], viewport={'zoom': 5})
 
-        self.assertEqual(layout._layout[0].get('camera'),
-                         {'bearing': None, 'center': None, 'pitch': None, 'zoom': 2})
-        self.assertEqual(layout._layout[1].get('camera'),
-                         {'bearing': None, 'center': None, 'pitch': None, 'zoom': 5})
+        assert layout._layout[0].get('camera') == {
+            'bearing': None, 'center': None, 'pitch': None, 'zoom': 2}
+        assert layout._layout[1].get('camera') == {
+            'bearing': None, 'center': None, 'pitch': None, 'zoom': 5}
 
     def test_is_static(self):
         """Layout should set correctly is_static property for each map"""
@@ -98,5 +99,5 @@ class TestLayoutSettings(unittest.TestCase):
             Map(Layer(Source(SOURCE)))
         ], viewport={'zoom': 5})
 
-        self.assertTrue(layout._layout[0].get('is_static'))
-        self.assertTrue(layout._layout[1].get('is_static'))
+        assert layout._layout[0].get('is_static') is True
+        assert layout._layout[1].get('is_static') is True
