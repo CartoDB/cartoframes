af81ee2b958ff008afe540e7d059c7ac845d5dfa
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2019-10-03 16:49:30 +0200

    Update style tests

--- tests/unit/viz/test_style.py
+++ tests/unit/viz/test_style.py
@@ -1,48 +1,49 @@
-import unittest
+import pytest
+
 from cartoframes.viz import Style
 
 
-class TestStyle(unittest.TestCase):
+class TestStyle(object):
     def test_is_style_defined(self):
         """Style"""
-        self.assertNotEqual(Style, None)
+        assert Style is not None
 
     def test_style_default_point(self):
         """Style.compute_viz should return the default viz for point"""
         style = Style()
         viz = style.compute_viz('point')
 
-        self.assertIn('color: hex("#EE4D5A")', viz)
-        self.assertIn('width: ramp(linear(zoom(),0,18),[2,10])', viz)
-        self.assertIn('strokeWidth: ramp(linear(zoom(),0,18),[0,1])', viz)
-        self.assertIn('strokeColor: opacity(#222,ramp(linear(zoom(),0,18),[0,0.6]))', viz)
+        assert 'color: hex("#EE4D5A")' in viz
+        assert 'width: ramp(linear(zoom(),0,18),[2,10])' in viz
+        assert 'strokeWidth: ramp(linear(zoom(),0,18),[0,1])' in viz
+        assert 'strokeColor: opacity(#222,ramp(linear(zoom(),0,18),[0,0.6]))' in viz
 
     def test_style_default_line(self):
         """Style.compute_viz should return the default viz for line"""
         style = Style()
         viz = style.compute_viz('line')
 
-        self.assertIn('color: hex("#4CC8A3")', viz)
-        self.assertIn('width: ramp(linear(zoom(),0,18),[0.5,4])', viz)
+        assert 'color: hex("#4CC8A3")' in viz
+        assert 'width: ramp(linear(zoom(),0,18),[0.5,4])' in viz
 
     def test_style_default_polygon(self):
         """Style.compute_viz should return the default viz for polygon"""
         style = Style()
         viz = style.compute_viz('polygon')
 
-        self.assertIn('color: hex("#826DBA")', viz)
-        self.assertIn('strokeWidth: ramp(linear(zoom(),2,18),[0.5,1])', viz)
-        self.assertIn('strokeColor: opacity(#2c2c2c,ramp(linear(zoom(),2,18),[0.2,0.6]))', viz)
+        assert 'color: hex("#826DBA")' in viz
+        assert 'strokeWidth: ramp(linear(zoom(),2,18),[0.5,1])' in viz
+        assert 'strokeColor: opacity(#2c2c2c,ramp(linear(zoom(),2,18),[0.2,0.6]))' in viz
 
     def test_style_string(self):
         """Style.compute_viz should return the viz from a string with defaults"""
         style = Style('@var: 1\ncolor: red')
         viz = style.compute_viz('line')
 
-        self.assertIn('@var: 1', viz)
-        self.assertIn('color: red', viz)
-        self.assertIn('width: ramp(linear(zoom(),0,18),[0.5,4])', viz)
-        self.assertNotIn('color: hex("#4CC8A3")', viz)
+        assert '@var: 1' in viz
+        assert 'color: red' in viz
+        assert 'width: ramp(linear(zoom(),0,18),[0.5,4])' in viz
+        assert 'color: hex("#4CC8A3")' not in viz
 
     def test_style_dict(self):
         """Style.compute_viz should return the viz from a dict with defaults"""
@@ -55,12 +56,12 @@ class TestStyle(unittest.TestCase):
         })
         viz = style.compute_viz('polygon')
 
-        self.assertIn('@var: 1', viz)
-        self.assertIn('color: red', viz)
-        self.assertIn('strokeWidth: 0', viz)
-        self.assertIn('strokeColor: opacity(#2c2c2c,ramp(linear(zoom(),2,18),[0.2,0.6]))', viz)
-        self.assertNotIn('color: hex("#826DBA")', viz)
-        self.assertNotIn('strokeWidth: ramp(linear(zoom(),2,18),[0.5,1])', viz)
+        assert '@var: 1' in viz
+        assert 'color: red' in viz
+        assert 'strokeWidth: 0' in viz
+        assert 'strokeColor: opacity(#2c2c2c,ramp(linear(zoom(),2,18),[0.2,0.6]))', viz
+        assert 'color: hex("#826DBA")' not in viz
+        assert 'strokeWidth: ramp(linear(zoom(),2,18),[0.5,1])' not in viz
 
     def test_style_default_variables(self):
         """Style.compute_viz should return the default viz with the variables"""
@@ -70,8 +71,8 @@ class TestStyle(unittest.TestCase):
             'momo': 123
         })
 
-        self.assertIn('@mimi: $pop', viz)
-        self.assertIn('@momo: 123', viz)
+        assert '@mimi: $pop' in viz
+        assert '@momo: 123' in viz
 
     def test_style_string_variables(self):
         """Style.compute_viz should return the string viz with the variables"""
@@ -81,9 +82,9 @@ class TestStyle(unittest.TestCase):
             'momo': 123
         })
 
-        self.assertIn('@var: 1', viz)
-        self.assertIn('@mimi: $pop', viz)
-        self.assertIn('@momo: 123', viz)
+        assert '@var: 1' in viz
+        assert '@mimi: $pop' in viz
+        assert '@momo: 123' in viz
 
     def test_style_dict_variables(self):
         """Style.compute_viz should return the dict viz with the variables"""
@@ -97,6 +98,6 @@ class TestStyle(unittest.TestCase):
             'momo': 123
         })
 
-        self.assertIn('@var: 1', viz)
-        self.assertIn('@mimi: $pop', viz)
-        self.assertIn('@momo: 123', viz)
+        assert '@var: 1' in viz
+        assert '@mimi: $pop' in viz
+        assert '@momo: 123' in viz
