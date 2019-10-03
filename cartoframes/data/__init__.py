7bb469f5d64eeefe5095246f48d923cc39b980e6
elenatorro <elenatorro@gmail.com>
2019-10-03 12:35:38 +0200

    Revert import

--- cartoframes/data/__init__.py
+++ cartoframes/data/__init__.py
@@ -1,7 +1,9 @@
 from __future__ import absolute_import
 
 from .dataset.dataset import Dataset
+from .dataset.registry.strategies_registry import StrategiesRegistry
 
 __all__ = [
-    'Dataset'
+    'Dataset',
+    'StrategiesRegistry',
 ]
