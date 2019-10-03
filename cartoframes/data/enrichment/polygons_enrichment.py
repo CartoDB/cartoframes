0cc26e582209ee13ff7260af9d0c5070a41533e0
Alejandro Hall <apolvillo@carto.com>
2019-10-03 20:47:45 +0200

    Suport several variables

--- cartoframes/data/enrichment/polygons_enrichment.py
+++ cartoframes/data/enrichment/polygons_enrichment.py
@@ -11,43 +11,50 @@ def enrich_polygons(data, variables, agg_operators, data_geom_column='geometry',
     return data_enriched
 
 
-def _prepare_sql(enrichment_id, filters_processed, variables_processed, enrichment_table,
-                 enrichment_geo_table, user_dataset, working_project, data_table, **kwargs):
+def _prepare_sql(enrichment_id, filters_processed, table_to_geotable, table_to_variables,
+                 table_to_project, table_to_dataset, user_dataset, working_project, data_table, **kwargs):
 
     grouper = 'group by data_table.{enrichment_id}'.format(enrichment_id=enrichment_id)
 
-    if 'agg_operators' in kwargs:
+    sqls = list()
 
-        if isinstance(kwargs['agg_operators'], str):
-            agg_operators = {variable: kwargs['agg_operators'] for variable in variables_processed}
+    for table, variables in table_to_variables.items():
+
+        if 'agg_operators' in kwargs:
+
+            if isinstance(kwargs['agg_operators'], str):
+                agg_operators = {variable: kwargs['agg_operators'] for variable in variables}
+            else:
+                agg_operators = kwargs['agg_operators']
+
+            variables_sql = ['{operator}({variable} * \
+                              (ST_Area(ST_Intersection(enrichment_geo_table.geom, data_table.{data_geom_column}))\
+                              / ST_area(data_table.{data_geom_column}))) as {variable}'.format(variable=variable,
+                             data_geom_column=kwargs['data_geom_column'],
+                            operator=agg_operators[variable]) for variable in variables]
+
         else:
-            agg_operators = kwargs['agg_operators']
+            variables_sql = variables + ['ST_Area(ST_Intersection(geo_table.geom, data_table.{data_geom_column}))\
+                                         / ST_area(data_table.{data_geom_column}) AS measures_proportion'.format(
+                                         data_geom_column=kwargs['data_geom_column'])]
+            grouper = ''
 
-        variables_sql = ['{operator}({variable} * \
-                         (ST_Area(ST_Intersection(enrichment_geo_table.geom, data_table.{data_geom_column}))\
-                         / ST_area(data_table.{data_geom_column}))) as {variable}'.format(variable=variable,
-                         data_geom_column=kwargs['data_geom_column'],
-                         operator=agg_operators[variable]) for variable in variables_processed]
+        sql = '''
+            SELECT data_table.{enrichment_id}, {variables}
+            FROM `{project}.{dataset}.{enrichment_table}` enrichment_table
+            JOIN `{project}.{dataset}.{enrichment_geo_table}` enrichment_geo_table
+            ON enrichment_table.geoid = enrichment_geo_table.geoid
+            JOIN `{working_project}.{user_dataset}.{data_table}` data_table
+            ON ST_Intersects(data_table.{data_geom_column}, enrichment_geo_table.geom)
+            {filters}
+            {grouper};
+        '''.format(enrichment_id=enrichment_id, variables=', '.join(variables_sql),
+                   enrichment_table=table, enrichment_geo_table=table_to_geotable[table],
+                   user_dataset=user_dataset, working_project=working_project,
+                   data_table=data_table, data_geom_column=kwargs['data_geom_column'],
+                   filters=filters_processed, grouper=grouper, project=table_to_project[table],
+                   dataset=table_to_dataset[table])
 
-    else:
-        variables_sql = variables_processed + ['ST_Area(ST_Intersection(geo_table.geom, data_table.{data_geom_column}))\
-                                               / ST_area(data_table.{data_geom_column}) AS measures_proportion'.format(
-                                                data_geom_column=kwargs['data_geom_column'])]
-        grouper = ''
+        sqls.append(sql)
 
-    sql = '''
-        SELECT data_table.{enrichment_id}, {variables}
-        FROM `{working_project}.{user_dataset}.{enrichment_table}` enrichment_table
-        JOIN `{working_project}.{user_dataset}.{enrichment_geo_table}` enrichment_geo_table
-          ON enrichment_table.geoid = enrichment_geo_table.geoid
-        JOIN `{working_project}.{user_dataset}.{data_table}` data_table
-          ON ST_Intersects(data_table.{data_geom_column}, enrichment_geo_table.geom)
-        {filters}
-        {grouper};
-    '''.format(enrichment_id=enrichment_id, variables=', '.join(variables_sql),
-               enrichment_table=enrichment_table, enrichment_geo_table=enrichment_geo_table,
-               user_dataset=user_dataset, working_project=working_project,
-               data_table=data_table, data_geom_column=kwargs['data_geom_column'],
-               filters=filters_processed, grouper=grouper)
-
-    return sql
+    return sqls
