2b7b373b11c6e4e93c7c62a5410b882a63dfdd2c
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2019-10-03 15:42:09 +0200

    Update layer tests

--- tests/unit/viz/test_layer.py
+++ tests/unit/viz/test_layer.py
@@ -1,61 +1,50 @@
-import unittest
-try:
-    from unittest.mock import Mock
-except ImportError:
-    from mock import Mock
+import pytest
 
 from cartoframes.viz import Layer, Source, Style, Popup, Legend
 
 
-class TestLayer(unittest.TestCase):
-    def setUp(self):
-        Source.get_geom_type = Mock(return_value='point')
-        self.orig_compute_query_bounds = Source._compute_query_bounds
-        Source._compute_query_bounds = Mock(return_valye=None)
+def setup_mocks(mocker):
+    mocker.patch.object(Source, 'get_geom_type', return_value='point')
+    mocker.patch.object(Source, '_compute_query_bounds')
 
-    def tearDown(self):
-        Source._compute_query_bounds = self.orig_compute_query_bounds
 
+class TestLayer(object):
     def test_is_layer_defined(self):
         """Layer"""
-        self.assertNotEqual(Layer, None)
+        assert Layer is not None
 
-    def test_initialization_objects(self):
+    def test_initialization_objects(self, mocker):
         """Layer should initialize layer attributes"""
+        setup_mocks(mocker)
         layer = Layer(Source('layer_source'))
 
-        self.assertFalse(layer.is_basemap)
-        self.assertEqual(layer.orig_query, 'SELECT * FROM "public"."layer_source"')
-        self.assertTrue(isinstance(layer.source, Source))
-        self.assertTrue(isinstance(layer.style, Style))
-        self.assertTrue(isinstance(layer.popup, Popup))
-        self.assertTrue(isinstance(layer.legend, Legend))
-        self.assertEqual(layer.interactivity, [])
+        assert layer.is_basemap is False
+        assert layer.orig_query == 'SELECT * FROM "public"."layer_source"'
+        assert isinstance(layer.source, Source)
+        assert isinstance(layer.style, Style)
+        assert isinstance(layer.popup, Popup)
+        assert isinstance(layer.legend, Legend)
+        assert layer.interactivity == []
 
-    def test_initialization_simple(self):
+    def test_initialization_simple(self, mocker):
         """Layer should initialize layer attributes"""
+        setup_mocks(mocker)
         layer = Layer('layer_source', '')
 
-        self.assertFalse(layer.is_basemap)
-        self.assertEqual(layer.orig_query, 'SELECT * FROM "public"."layer_source"')
-        self.assertTrue(isinstance(layer.source, Source))
-        self.assertTrue(isinstance(layer.style, Style))
-        self.assertTrue(isinstance(layer.popup, Popup))
-        self.assertTrue(isinstance(layer.legend, Legend))
-        self.assertEqual(layer.interactivity, [])
+        assert layer.is_basemap is False
+        assert layer.orig_query == 'SELECT * FROM "public"."layer_source"'
+        assert isinstance(layer.source, Source)
+        assert isinstance(layer.style, Style)
+        assert isinstance(layer.popup, Popup)
+        assert isinstance(layer.legend, Legend)
+        assert layer.interactivity == []
 
 
-class TestLayerStyle(unittest.TestCase):
-    def setUp(self):
-        Source.get_geom_type = Mock(return_value='point')
-        self.orig_compute_query_bounds = Source._compute_query_bounds
-        Source._compute_query_bounds = Mock(return_valye=None)
+class TestLayerStyle(object):
 
-    def tearDown(self):
-        Source._compute_query_bounds = self.orig_compute_query_bounds
-
-    def test_style_dict(self):
+    def test_style_dict(self, mocker):
         """Layer style should set the style when it is a dict"""
+        setup_mocks(mocker)
         layer = Layer(
             'layer_source',
             {
@@ -69,15 +58,16 @@ class TestLayerStyle(unittest.TestCase):
             }
         )
 
-        self.assertTrue(isinstance(layer.style, Style))
-        self.assertTrue('@grad: [red, green, blue]' in layer.viz)
-        self.assertTrue('color: blue' in layer.viz)
-        self.assertTrue('width: 10' in layer.viz)
-        self.assertTrue('strokeColor: black' in layer.viz)
-        self.assertTrue('strokeWidth: 1' in layer.viz)
+        assert isinstance(layer.style, Style)
+        assert '@grad: [red, green, blue]' in layer.viz
+        assert 'color: blue' in layer.viz
+        assert 'width: 10' in layer.viz
+        assert 'strokeColor: black' in layer.viz
+        assert 'strokeWidth: 1' in layer.viz
 
-    def test_style_str(self):
+    def test_style_str(self, mocker):
         """Layer style should set the style when it is a dict"""
+        setup_mocks(mocker)
         layer = Layer(
             'layer_source',
             """
@@ -89,9 +79,9 @@ class TestLayerStyle(unittest.TestCase):
             """
         )
 
-        self.assertTrue(isinstance(layer.style, Style))
-        self.assertTrue('@grad: [red, green, blue]' in layer.viz)
-        self.assertTrue('color: blue' in layer.viz)
-        self.assertTrue('width: 10' in layer.viz)
-        self.assertTrue('strokeColor: black' in layer.viz)
-        self.assertTrue('strokeWidth: 1' in layer.viz)
+        assert isinstance(layer.style, Style)
+        assert '@grad: [red, green, blue]' in layer.viz
+        assert 'color: blue' in layer.viz
+        assert 'width: 10' in layer.viz
+        assert 'strokeColor: black' in layer.viz
+        assert 'strokeWidth: 1' in layer.viz
