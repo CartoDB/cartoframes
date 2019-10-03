1cbacfa67fcfe9c56bd0641967a33505b00dbf15
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2019-10-03 18:27:55 +0200

    Update size helpers tests

--- tests/unit/viz/helpers/test_size_bins_layer.py
+++ tests/unit/viz/helpers/test_size_bins_layer.py
@@ -1,52 +1,45 @@
-import unittest
-try:
-    from unittest.mock import Mock
-except ImportError:
-    from mock import Mock
-from cartoframes.viz import helpers, Source
+import pytest
+
+from cartoframes.viz import helpers
+
+from . import setup_mocks
 
 
-class TestSizeBinsLayerHelper(unittest.TestCase):
-    def setUp(self):
-        self.orig_compute_query_bounds = Source._compute_query_bounds
-        Source._compute_query_bounds = Mock(return_valye=None)
-
-    def tearDown(self):
-        Source._compute_query_bounds = self.orig_compute_query_bounds
-
+class TestSizeBinsLayerHelper(object):
     def test_helpers(self):
         "should be defined"
-        self.assertNotEqual(helpers.size_bins_layer, None)
+        assert helpers.size_bins_layer is not None
 
-    def test_size_bins_layer(self):
+    def test_size_bins_layer(self, mocker):
         "should create a layer with the proper attributes"
-        Source._get_geom_type = Mock(return_value='point')
-
+        setup_mocks(mocker)
         layer = helpers.size_bins_layer(
             source='sf_neighborhoods',
             value='name'
         )
 
-        self.assertNotEqual(layer.style, None)
-        self.assertEqual(layer.style._style['point']['width'], 'ramp(globalQuantiles($name, 5), [2, 14])')
-        self.assertEqual(layer.style._style['line']['width'], 'ramp(globalQuantiles($name, 5), [1, 10])')
-        self.assertEqual(layer.style._style['point']['color'], 'opacity(#EE4D5A, 0.8)')
-        self.assertEqual(layer.style._style['line']['color'], 'opacity(#4CC8A3, 0.8)')
-        self.assertNotEqual(layer.popup, None)
-        self.assertEqual(layer.popup._hover, [{
+        assert layer.style is not None
+        assert layer.style._style['point']['width'] == 'ramp(globalQuantiles($name, 5), [2, 14])'
+        assert layer.style._style['line']['width'] == 'ramp(globalQuantiles($name, 5), [1, 10])'
+        assert layer.style._style['point']['color'] == 'opacity(#EE4D5A, 0.8)'
+        assert layer.style._style['line']['color'] == 'opacity(#4CC8A3, 0.8)'
+
+        assert layer.popup is not None
+        assert layer.popup._hover == [{
             'title': 'name',
             'value': '$name'
-        }])
+        }]
 
-        self.assertNotEqual(layer.legend, None)
-        self.assertEqual(layer.legend._type['point'], 'size-bins-point')
-        self.assertEqual(layer.legend._type['line'], 'size-bins-line')
-        self.assertEqual(layer.legend._title, 'name')
-        self.assertEqual(layer.legend._description, '')
-        self.assertEqual(layer.legend._footer, '')
+        assert layer.legend is not None
+        assert layer.legend._type['point'] == 'size-bins-point'
+        assert layer.legend._type['line'] == 'size-bins-line'
+        assert layer.legend._title == 'name'
+        assert layer.legend._description == ''
+        assert layer.legend._footer == ''
 
-    def test_size_bins_layer_point(self):
+    def test_size_bins_layer_point(self, mocker):
         "should create a point type layer"
+        setup_mocks(mocker)
         layer = helpers.size_bins_layer(
             'sf_neighborhoods',
             'name',
@@ -56,19 +49,12 @@ class TestSizeBinsLayerHelper(unittest.TestCase):
             color='blue'
         )
 
-        self.assertEqual(
-            layer.style._style['point']['width'],
-            'ramp(globalQuantiles($name, 3), [10, 20])'
-        )
-        self.assertEqual(
-            layer.style._style['point']['color'],
-            'opacity(blue, 0.8)'
-        )
+        assert layer.style._style['point']['width'] == 'ramp(globalQuantiles($name, 3), [10, 20])'
+        assert layer.style._style['point']['color'] == 'opacity(blue, 0.8)'
 
-    def test_size_bins_layer_line(self):
+    def test_size_bins_layer_line(self, mocker):
         "should create a line type layer"
-        Source._get_geom_type = Mock(return_value='line')
-
+        setup_mocks(mocker, 'line')
         layer = helpers.size_bins_layer(
             'sf_neighborhoods',
             'name',
@@ -78,31 +64,20 @@ class TestSizeBinsLayerHelper(unittest.TestCase):
             color='blue'
         )
 
-        self.assertEqual(
-            layer.style._style['line']['width'],
-            'ramp(globalQuantiles($name, 3), [10, 20])'
-        )
-        self.assertEqual(
-            layer.style._style['line']['color'],
-            'opacity(blue, 0.8)'
-        )
+        assert layer.style._style['line']['width'] == 'ramp(globalQuantiles($name, 3), [10, 20])'
+        assert layer.style._style['line']['color'] == 'opacity(blue, 0.8)'
 
-    def test_size_bins_layer_method(self):
+    def test_size_bins_layer_method(self, mocker):
         "should apply the classification method"
+        setup_mocks(mocker)
         layer = helpers.size_bins_layer(
             'sf_neighborhoods',
             'name',
             method='quantiles'
         )
 
-        self.assertEqual(
-            layer.style._style['point']['width'],
-            'ramp(globalQuantiles($name, 5), [2, 14])'
-        )
-        self.assertEqual(
-            layer.style._style['line']['width'],
-            'ramp(globalQuantiles($name, 5), [1, 10])'
-        )
+        assert layer.style._style['point']['width'] == 'ramp(globalQuantiles($name, 5), [2, 14])'
+        assert layer.style._style['line']['width'] == 'ramp(globalQuantiles($name, 5), [1, 10])'
 
         layer = helpers.size_bins_layer(
             'sf_neighborhoods',
@@ -110,14 +85,8 @@ class TestSizeBinsLayerHelper(unittest.TestCase):
             method='equal'
         )
 
-        self.assertEqual(
-            layer.style._style['point']['width'],
-            'ramp(globalEqIntervals($name, 5), [2, 14])'
-        )
-        self.assertEqual(
-            layer.style._style['line']['width'],
-            'ramp(globalEqIntervals($name, 5), [1, 10])'
-        )
+        assert layer.style._style['point']['width'] == 'ramp(globalEqIntervals($name, 5), [2, 14])'
+        assert layer.style._style['line']['width'] == 'ramp(globalEqIntervals($name, 5), [1, 10])'
 
         layer = helpers.size_bins_layer(
             'sf_neighborhoods',
@@ -125,52 +94,41 @@ class TestSizeBinsLayerHelper(unittest.TestCase):
             method='stdev'
         )
 
-        self.assertEqual(
-            layer.style._style['point']['width'],
-            'ramp(globalStandardDev($name, 5), [2, 14])'
-        )
-        self.assertEqual(
-            layer.style._style['line']['width'],
-            'ramp(globalStandardDev($name, 5), [1, 10])'
-        )
+        assert layer.style._style['point']['width'] == 'ramp(globalStandardDev($name, 5), [2, 14])'
+        assert layer.style._style['line']['width'] == 'ramp(globalStandardDev($name, 5), [1, 10])'
 
         msg = 'Available methods are: "quantiles", "equal", "stdev".'
-        with self.assertRaisesRegexp(ValueError, msg):
+        with pytest.raises(ValueError) as e:
             helpers.size_bins_layer(
                 'sf_neighborhoods',
                 'name',
                 method='wrong'
             )
+        assert str(e.value) == msg
 
-    def test_size_bins_layer_breaks(self):
+    def test_size_bins_layer_breaks(self, mocker):
         "should apply buckets if breaks are passed"
-        Source._get_geom_type = Mock(return_value='point')
-
+        setup_mocks(mocker)
         layer = helpers.size_bins_layer(
             'sf_neighborhoods',
             'name',
             breaks=[0, 1, 2]
         )
 
-        self.assertEqual(
-            layer.style._style['point']['width'],
-            'ramp(buckets($name, [0, 1, 2]), [2, 14])'
-        )
-        self.assertEqual(
-            layer.style._style['line']['width'],
-            'ramp(buckets($name, [0, 1, 2]), [1, 10])'
-        )
+        assert layer.style._style['point']['width'] == 'ramp(buckets($name, [0, 1, 2]), [2, 14])'
+        assert layer.style._style['line']['width'] == 'ramp(buckets($name, [0, 1, 2]), [1, 10])'
 
-    def test_size_bins_layer_legend(self):
+    def test_size_bins_layer_legend(self, mocker):
         "should show/hide the legend"
+        setup_mocks(mocker)
         layer = helpers.size_bins_layer(
             'sf_neighborhoods',
             'name',
             legend=False
         )
 
-        self.assertEqual(layer.legend._type, '')
-        self.assertEqual(layer.legend._title, '')
+        assert layer.legend._type == ''
+        assert layer.legend._title == ''
 
         layer = helpers.size_bins_layer(
             'sf_neighborhoods',
@@ -178,22 +136,23 @@ class TestSizeBinsLayerHelper(unittest.TestCase):
             legend=True
         )
 
-        self.assertEqual(layer.legend._type, {
+        assert layer.legend._type == {
             'point': 'size-bins-point',
             'line': 'size-bins-line',
             'polygon': 'size-bins-polygon'
-        })
-        self.assertEqual(layer.legend._title, 'name')
+        }
+        assert layer.legend._title == 'name'
 
-    def test_size_bins_layer_popup(self):
+    def test_size_bins_layer_popup(self, mocker):
         "should show/hide the popup"
+        setup_mocks(mocker)
         layer = helpers.size_bins_layer(
             'sf_neighborhoods',
             'name',
             popup=False
         )
 
-        self.assertEqual(layer.popup._hover, [])
+        assert layer.popup._hover == []
 
         layer = helpers.size_bins_layer(
             'sf_neighborhoods',
@@ -201,20 +160,21 @@ class TestSizeBinsLayerHelper(unittest.TestCase):
             popup=True
         )
 
-        self.assertEqual(layer.popup._hover, [{
+        assert layer.popup._hover == [{
             'title': 'name',
             'value': '$name'
-        }])
+        }]
 
-    def test_size_bins_layer_widget(self):
+    def test_size_bins_layer_widget(self, mocker):
         "should show/hide the widget"
+        setup_mocks(mocker)
         layer = helpers.size_bins_layer(
             'sf_neighborhoods',
             'name',
             widget=False
         )
 
-        self.assertEqual(layer.widgets._widgets, [])
+        assert layer.widgets._widgets == []
 
         layer = helpers.size_bins_layer(
             'sf_neighborhoods',
@@ -222,18 +182,19 @@ class TestSizeBinsLayerHelper(unittest.TestCase):
             widget=True
         )
 
-        self.assertEqual(layer.widgets._widgets[0]._type, 'histogram')
-        self.assertEqual(layer.widgets._widgets[0]._title, 'Distribution')
+        assert layer.widgets._widgets[0]._type == 'histogram'
+        assert layer.widgets._widgets[0]._title == 'Distribution'
 
-    def test_size_bins_layer_animate(self):
+    def test_size_bins_layer_animate(self, mocker):
         "should animate a property and disable the popups"
+        setup_mocks(mocker)
         layer = helpers.size_bins_layer(
             'sf_neighborhoods',
             'name',
             animate='time'
         )
 
-        self.assertEqual(layer.popup._hover, [])
-        self.assertEqual(layer.widgets._widgets[0]._type, 'time-series')
-        self.assertEqual(layer.widgets._widgets[0]._title, 'Animation')
-        self.assertEqual(layer.widgets._widgets[0]._value, 'time')
+        assert layer.popup._hover == []
+        assert layer.widgets._widgets[0]._type == 'time-series'
+        assert layer.widgets._widgets[0]._title == 'Animation'
+        assert layer.widgets._widgets[0]._value == 'time'
--- tests/unit/viz/helpers/test_size_category_layer.py
+++ tests/unit/viz/helpers/test_size_category_layer.py
@@ -1,53 +1,46 @@
-import unittest
-try:
-    from unittest.mock import Mock
-except ImportError:
-    from mock import Mock
-from cartoframes.viz import helpers, Source
+import pytest
+
+from cartoframes.viz import helpers
+
+from . import setup_mocks
 
 
-class TestSizeCategoryLayerHelper(unittest.TestCase):
-    def setUp(self):
-        self.orig_compute_query_bounds = Source._compute_query_bounds
-        Source._compute_query_bounds = Mock(return_valye=None)
-
-    def tearDown(self):
-        Source._compute_query_bounds = self.orig_compute_query_bounds
-
+class TestSizeCategoryLayerHelper(object):
     def test_helpers(self):
         "should be defined"
-        self.assertNotEqual(helpers.size_category_layer, None)
+        assert helpers.size_category_layer is not None
 
-    def test_size_category_layer(self):
+    def test_size_category_layer(self, mocker):
         "should create a layer with the proper attributes"
-        Source._get_geom_type = Mock(return_value='point')
-
+        setup_mocks(mocker)
         layer = helpers.size_category_layer(
             source='sf_neighborhoods',
             value='name',
             title='Neighborhoods'
         )
 
-        self.assertNotEqual(layer.style, None)
-        self.assertEqual(layer.style._style['point']['width'], 'ramp(top($name, 5), [2, 20])')
-        self.assertEqual(layer.style._style['line']['width'], 'ramp(top($name, 5), [1, 10])')
-        self.assertEqual(layer.style._style['point']['color'], 'opacity(#F46D43, 0.8)')
-        self.assertEqual(layer.style._style['line']['color'], 'opacity(#4CC8A3, 0.8)')
-        self.assertNotEqual(layer.popup, None)
-        self.assertEqual(layer.popup._hover, [{
+        assert layer.style is not None
+        assert layer.style._style['point']['width'] == 'ramp(top($name, 5), [2, 20])'
+        assert layer.style._style['line']['width'] == 'ramp(top($name, 5), [1, 10])'
+        assert layer.style._style['point']['color'] == 'opacity(#F46D43, 0.8)'
+        assert layer.style._style['line']['color'] == 'opacity(#4CC8A3, 0.8)'
+
+        assert layer.popup is not None
+        assert layer.popup._hover == [{
             'title': 'Neighborhoods',
             'value': '$name'
-        }])
+        }]
 
-        self.assertNotEqual(layer.legend, None)
-        self.assertEqual(layer.legend._type['point'], 'size-category-point')
-        self.assertEqual(layer.legend._type['line'], 'size-category-line')
-        self.assertEqual(layer.legend._title, 'Neighborhoods')
-        self.assertEqual(layer.legend._description, '')
-        self.assertEqual(layer.legend._footer, '')
+        assert layer.legend is not None
+        assert layer.legend._type['point'] == 'size-category-point'
+        assert layer.legend._type['line'] == 'size-category-line'
+        assert layer.legend._title == 'Neighborhoods'
+        assert layer.legend._description == ''
+        assert layer.legend._footer == ''
 
-    def test_size_category_layer_point(self):
+    def test_size_category_layer_point(self, mocker):
         "should create a point type layer"
+        setup_mocks(mocker)
         layer = helpers.size_category_layer(
             'sf_neighborhoods',
             'name',
@@ -57,14 +50,8 @@ class TestSizeCategoryLayerHelper(unittest.TestCase):
             color='blue'
         )
 
-        self.assertEqual(
-            layer.style._style['point']['width'],
-            'ramp(top($name, 5), [10, 20])'
-        )
-        self.assertEqual(
-            layer.style._style['point']['color'],
-            'opacity(blue, 0.8)'
-        )
+        assert layer.style._style['point']['width'] == 'ramp(top($name, 5), [10, 20])'
+        assert layer.style._style['point']['color'] == 'opacity(blue, 0.8)'
 
         layer = helpers.size_category_layer(
             'sf_neighborhoods',
@@ -75,19 +62,12 @@ class TestSizeCategoryLayerHelper(unittest.TestCase):
             color='blue'
         )
 
-        self.assertEqual(
-            layer.style._style['point']['width'],
-            "ramp(buckets($name, ['A', 'B']), [10, 20])"
-        )
-        self.assertEqual(
-            layer.style._style['point']['color'],
-            'opacity(blue, 0.8)'
-        )
+        assert layer.style._style['point']['width'] == "ramp(buckets($name, ['A', 'B']), [10, 20])"
+        assert layer.style._style['point']['color'] == 'opacity(blue, 0.8)'
 
-    def test_size_category_layer_line(self):
+    def test_size_category_layer_line(self, mocker):
         "should create a line type layer"
-        Source._get_geom_type = Mock(return_value='line')
-
+        setup_mocks(mocker, 'line')
         layer = helpers.size_category_layer(
             'sf_neighborhoods',
             'name',
@@ -97,14 +77,8 @@ class TestSizeCategoryLayerHelper(unittest.TestCase):
             color='blue'
         )
 
-        self.assertEqual(
-            layer.style._style['line']['width'],
-            'ramp(top($name, 5), [10, 20])'
-        )
-        self.assertEqual(
-            layer.style._style['line']['color'],
-            'opacity(blue, 0.8)'
-        )
+        assert layer.style._style['line']['width'] == 'ramp(top($name, 5), [10, 20])'
+        assert layer.style._style['line']['color'] == 'opacity(blue, 0.8)'
 
         layer = helpers.size_category_layer(
             'sf_neighborhoods',
@@ -115,25 +89,20 @@ class TestSizeCategoryLayerHelper(unittest.TestCase):
             color='blue'
         )
 
-        self.assertEqual(
-            layer.style._style['line']['width'],
-            "ramp(buckets($name, ['A', 'B']), [10, 20])"
-        )
-        self.assertEqual(
-            layer.style._style['line']['color'],
-            'opacity(blue, 0.8)'
-        )
+        assert layer.style._style['line']['width'] == "ramp(buckets($name, ['A', 'B']), [10, 20])"
+        assert layer.style._style['line']['color'] == 'opacity(blue, 0.8)'
 
-    def test_size_category_layer_legend(self):
+    def test_size_category_layer_legend(self, mocker):
         "should show/hide the legend"
+        setup_mocks(mocker)
         layer = helpers.size_category_layer(
             'sf_neighborhoods',
             'name',
             legend=False
         )
 
-        self.assertEqual(layer.legend._type, '')
-        self.assertEqual(layer.legend._title, '')
+        assert layer.legend._type == ''
+        assert layer.legend._title == ''
 
         layer = helpers.size_category_layer(
             'sf_neighborhoods',
@@ -141,22 +110,23 @@ class TestSizeCategoryLayerHelper(unittest.TestCase):
             legend=True
         )
 
-        self.assertEqual(layer.legend._type, {
+        assert layer.legend._type == {
             'point': 'size-category-point',
             'line': 'size-category-line',
             'polygon': 'size-category-polygon'
-        })
-        self.assertEqual(layer.legend._title, 'name')
+        }
+        assert layer.legend._title == 'name'
 
-    def test_size_category_layer_popup(self):
+    def test_size_category_layer_popup(self, mocker):
         "should show/hide the popup"
+        setup_mocks(mocker)
         layer = helpers.size_category_layer(
             'sf_neighborhoods',
             'name',
             popup=False
         )
 
-        self.assertEqual(layer.popup._hover, [])
+        assert layer.popup._hover == []
 
         layer = helpers.size_category_layer(
             'sf_neighborhoods',
@@ -164,20 +134,21 @@ class TestSizeCategoryLayerHelper(unittest.TestCase):
             popup=True
         )
 
-        self.assertEqual(layer.popup._hover, [{
+        assert layer.popup._hover, [{
             'title': 'name',
             'value': '$name'
-        }])
+        }]
 
-    def test_size_category_layer_widget(self):
+    def test_size_category_layer_widget(self, mocker):
         "should show/hide the widget"
+        setup_mocks(mocker)
         layer = helpers.size_category_layer(
             'sf_neighborhoods',
             'name',
             widget=False
         )
 
-        self.assertEqual(layer.widgets._widgets, [])
+        assert layer.widgets._widgets == []
 
         layer = helpers.size_category_layer(
             'sf_neighborhoods',
@@ -185,18 +156,19 @@ class TestSizeCategoryLayerHelper(unittest.TestCase):
             widget=True
         )
 
-        self.assertEqual(layer.widgets._widgets[0]._type, 'category')
-        self.assertEqual(layer.widgets._widgets[0]._title, 'Categories')
+        assert layer.widgets._widgets[0]._type == 'category'
+        assert layer.widgets._widgets[0]._title == 'Categories'
 
-    def test_size_category_layer_animate(self):
+    def test_size_category_layer_animate(self, mocker):
         "should animate a property and disable the popups"
+        setup_mocks(mocker)
         layer = helpers.size_category_layer(
             'sf_neighborhoods',
             'name',
             animate='time'
         )
 
-        self.assertEqual(layer.popup._hover, [])
-        self.assertEqual(layer.widgets._widgets[0]._type, 'time-series')
-        self.assertEqual(layer.widgets._widgets[0]._title, 'Animation')
-        self.assertEqual(layer.widgets._widgets[0]._value, 'time')
+        assert layer.popup._hover == []
+        assert layer.widgets._widgets[0]._type == 'time-series'
+        assert layer.widgets._widgets[0]._title == 'Animation'
+        assert layer.widgets._widgets[0]._value == 'time'
