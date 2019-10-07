3155279754f7b9a379caa1ef1a466e152e5c0487
elenatorro <elenatorro@gmail.com>
2019-10-07 18:23:27 +0200

    Remove DatasetInfo

--- cartoframes/data/__init__.py
+++ cartoframes/data/__init__.py
@@ -1,11 +1,9 @@
 from __future__ import absolute_import
 
 from .dataset.dataset import Dataset
-from .dataset.dataset_info import DatasetInfo
 from .dataset.registry.strategies_registry import StrategiesRegistry
 
 __all__ = [
     'Dataset',
-    'DatasetInfo',
     'StrategiesRegistry',
 ]
