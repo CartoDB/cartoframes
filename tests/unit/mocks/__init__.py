cd11c6fa33ca5dd422e4a1bdd39f96ddcb1ddfae
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2019-10-07 09:53:40 +0200

    Ignore indentation error

--- tests/unit/mocks/__init__.py
+++ tests/unit/mocks/__init__.py
@@ -2,6 +2,7 @@ from .context_mock import ContextMock
 from .dataset_mock import DatasetMock
 from .kuviz_mock import CartoKuvizMock
 
+
 def mock_create_context(mocker, response=''):
     context_mock = ContextMock(response)
     mocker.spy(context_mock, 'execute_query')
@@ -10,9 +11,11 @@ def mock_create_context(mocker, response=''):
         return_value=context_mock)
     return context_mock
 
+
 def mock_dataset(mocker, source, credentials=None):
     mock_create_context(mocker)
     return DatasetMock(source, credentials=credentials)
+
 
 def mock_kuviz(name, html, credentials=None, password=None):
     return CartoKuvizMock(name=name, password=password)
