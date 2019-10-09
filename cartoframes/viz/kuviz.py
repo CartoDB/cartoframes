acc1e9481d5ce7e10903d11eb951b1d9994b9964
Simon Martín <oleurud@gmail.com>
2019-10-09 18:42:33 +0200

    kuviz easy get and set layers

--- cartoframes/viz/kuviz.py
+++ cartoframes/viz/kuviz.py
@@ -15,16 +15,13 @@ DEFAULT_PUBLIC = 'default_public'
 
 
 class KuvizPublisher(object):
-    def __init__(self, layers, table_name=None, credentials=None):
+    def __init__(self, credentials=None):
         self.kuviz = None
         self._maps_api_key = DEFAULT_PUBLIC
 
-        self._table_name = table_name
         self._credentials = credentials or get_default_credentials()
         self._auth_client = _create_auth_client(self._credentials)
         self._layers = []
-
-        self._initialize(deepcopy(layers))
 
     @staticmethod
     def all(credentials=None):
@@ -33,42 +30,38 @@ class KuvizPublisher(object):
         kuvizs = km.all()
         return [kuviz_to_dict(kuviz) for kuviz in kuvizs]
 
-    def _initialize(self, layers):
+    def get_layers(self):
+        return self._layers
+
+    def set_layers(self, layers, table_name):
         self._sync_layers(layers)
         self._manage_maps_api_key()
+        self._add_layers_credentials()
 
     def publish(self, html, name, password=None):
         self.kuviz = _create_kuviz(html=html, name=name, auth_client=self._auth_client, password=password)
         return kuviz_to_dict(self.kuviz)
 
-    def get_layers(self):
-        for layer in self._layers:
-            if not layer.source.dataset.credentials:
-                layer.source.dataset.credentials = self._credentials
-
-            layer.credentials = {
-                'username': layer.source.dataset.credentials.username,
-                'api_key': self._maps_api_key,
-                'base_url': layer.source.dataset.credentials.base_url
-            }
-
-        return self._layers
+    def delete(self):
+        if self.kuviz:
+            self.kuviz.delete()
+            warn("Publication '{n}' ({id}) deleted".format(n=self._publisher.kuviz.name, id=self._publisher.kuviz.id))
+            self.kuviz = None
 
     def _sync_layers(self, layers):
         for idx, layer in enumerate(layers):
             table_name = normalize_name("{name}_{idx}".format(name=self._table_name, idx=idx))
-            layer = self._sync_layer(layer, table_name)
-
-        self._layers.append(layer)
+            if layer.source.dataset.is_local():
+                layer = self._sync_layer(layer, table_name)
+            self._layers.append(layer)
 
     def _sync_layer(self, layer, table_name):
-        if layer.source.dataset.is_local():
-            layer.source.dataset.upload(table_name=table_name, credentials=self._credentials)
-            layer.source = Source(table_name, credentials=self._credentials)
-            warn('Table `{}` created. In order to publish the map, you will need to create a new Regular API '
-                 'key with permissions to Maps API and the table `{}`. You can do it from your CARTO dashboard or '
-                 'using the Auth API. You can get more info at '
-                 'https://carto.com/developers/auth-api/guides/types-of-API-Keys/'.format(table_name, table_name))
+        layer.source.dataset.upload(table_name=table_name, credentials=self._credentials)
+        layer.source = Source(table_name, credentials=self._credentials)
+        warn('Table `{}` created. In order to publish the map, you will need to create a new Regular API '
+             'key with permissions to Maps API and the table `{}`. You can do it from your CARTO dashboard or '
+             'using the Auth API. You can get more info at '
+             'https://carto.com/developers/auth-api/guides/types-of-API-Keys/'.format(table_name, table_name))
 
         return layer
 
@@ -81,6 +74,14 @@ class KuvizPublisher(object):
             api_key_name = '{}_api_key'.format(self._table_name)
             auth_api_client = AuthAPIClient(self._credentials)
             self._maps_api_key = auth_api_client.create_api_key(non_public_datasets, api_key_name, ['maps'])
+
+    def _add_layers_credentials(self):
+        for layer in self._layers:
+            layer.credentials = {
+                'username': layer.source.dataset.credentials.username,
+                'api_key': self._maps_api_key,
+                'base_url': layer.source.dataset.credentials.base_url
+            }
 
 
 def _create_kuviz(html, name, auth_client, password=None):
