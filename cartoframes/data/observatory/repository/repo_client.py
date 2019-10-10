3aec1321643425c6bdb2a11f543155c309b7198f
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2019-10-10 13:20:36 +0200

    Extract fetch_subscriptions to utils

--- cartoframes/data/observatory/repository/repo_client.py
+++ cartoframes/data/observatory/repository/repo_client.py
@@ -1,9 +1,8 @@
 from __future__ import absolute_import
 
-from carto.do_datasets import DODatasetManager
-
 from ...clients import SQLClient
 from ....auth import Credentials
+from ..utils import get_subscription_ids
 
 
 class RepoClient(object):
@@ -47,7 +46,8 @@ class RepoClient(object):
 
         extra_condition = []
         if self._user_credentials is not None:
-            extra_condition.append('view.id IN ({})'.format(self._get_purchased_dataset_ids()))
+            ids = get_subscription_ids(self._user_credentials)
+            extra_condition.append('view.id IN ({})'.format(ids))
 
         return self._run_query(query, filters, extra_condition)
 
@@ -60,7 +60,8 @@ class RepoClient(object):
 
         extra_condition = []
         if self._user_credentials is not None:
-            extra_condition.append('view.id IN ({})'.format(self._get_purchased_dataset_ids()))
+            ids = get_subscription_ids(self._user_credentials)
+            extra_condition.append('view.id IN ({})'.format(ids))
 
         return self._run_query(query, filters, extra_condition)
 
@@ -80,18 +81,6 @@ class RepoClient(object):
             conditions.extend(["view.{} = '{}'".format(key, value) for key, value in filters.items()])
 
         return conditions
-
-    def _get_purchased_dataset_ids(self):
-        purchased_datasets = self._fetch_purchased_datasets()
-        purchased_dataset_ids = list(map(lambda pd: pd.id, purchased_datasets))
-        return ','.join(["'" + id + "'" for id in purchased_dataset_ids])
-
-    def _fetch_purchased_datasets(self):
-        api_key_auth_client = self._user_credentials.get_api_key_auth_client()
-        do_manager = DODatasetManager(api_key_auth_client)
-        if do_manager is not None:
-            return do_manager.all()
-        return []
 
     def __new__(cls):
         if not RepoClient.__instance:
--- cartoframes/data/observatory/utils.py
+++ cartoframes/data/observatory/utils.py
@@ -1,7 +1,19 @@
-message = '''
-<h3>Subscription contract</h3>
-You are about to subscribe to <b>%{dataset_name}</b>. The cost of this dataset is <b>$%{price}/%{update_frequency}</b>. If you want to proceed, a Request will be sent to CARTO who will order the data and load it into your account. This dataset is available for Instant Order for your organization, so it will automatically process the order and you will get inmediate access to the dataset. In order to proceed we need you to agree to the License of the dataset available at http://adasdasd.<br>Do you want to proceed?
-'''
+from carto.do_subscriptions import DOSubscriptionManager
+
+
+def get_subscription_ids(credentials):
+    purchased_datasets = fetch_subscriptions(credentials)
+    purchased_dataset_ids = list(map(lambda pd: pd.id, purchased_datasets))
+    return ','.join(["'" + id + "'" for id in purchased_dataset_ids])
+
+
+def fetch_subscriptions(credentials):
+    if credentials:
+        api_key_auth_client = credentials.get_api_key_auth_client()
+        do_manager = DOSubscriptionManager(api_key_auth_client)
+        if do_manager is not None:
+            return do_manager.all()
+    return []
 
 
 def display_subscription_form():
@@ -18,6 +30,15 @@ def display_notebook_subscription_form():
     def create_expanded_button(description, button_style):
         return Button(description=description, button_style=button_style, layout=Layout(height='32px', width='176px'))
 
+    message = '''
+    <h3>Subscription contract</h3>
+    You are about to subscribe to <b>%{dataset_name}</b>.
+    The cost of this dataset is <b>$%{price}/%{update_frequency}</b>.
+    If you want to proceed, a Request will be sent to CARTO who will order the data and load it into your account.
+    This dataset is available for Instant Order for your organization, so it will automatically process the order and you will get inmediate access to the dataset.
+    In order to proceed we need you to agree to the License of the dataset available at <b><a href=http://carto.com>http://carto.com</a></b>.
+    <br>Do you want to proceed?
+    '''
     text = HTML(message)
 
     button_yes = create_expanded_button('Yes', 'info')
@@ -46,7 +67,7 @@ def display_notebook_subscription_form():
 
 
 def display_cli_subscription_form():
-    pass
+    print('This method is not yet implemented in CLI')
 
 
 def is_ipython_notebook():
