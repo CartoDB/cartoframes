c54d42c806729ad5ca6d673dac90c211f08f36b3
Javier Goizueta <jgoizueta@gmail.com>
2019-10-11 19:51:47 +0200

    Fix typos

--- cartoframes/data/observatory/subscriptions.py
+++ cartoframes/data/observatory/subscriptions.py
@@ -2,12 +2,12 @@
 class Subscriptions(object):
 
     def __init__(self, datasets, geographies):
-        self._subscriptions_datasetes = datasets
+        self._subscriptions_datasets = datasets
         self._subscriptions_geographies = geographies
 
     @property
     def datasets(self):
-        return self._subscriptions_datasetes
+        return self._subscriptions_datasets
 
     @property
     def geographies(self):
