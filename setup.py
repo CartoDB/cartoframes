d60b6da66f2d7c1611e90c10512cfc83a2338967
juanrmn <juanr.gonzalez@gmail.com>
2019-10-23 10:52:36 +0200

    moving old-examples to examples/debug

--- setup.py
+++ setup.py
@@ -27,7 +27,8 @@ REQUIRES = [
     'unidecode>=1.1.0,<2.0',
     'pyarrow>=0.14.1,<1.0',
     'google-cloud-bigquery>=1.19.0,<2.0',
-    'geojson>=2.5.0,<3.0'
+    'geojson>=2.5.0,<3.0',
+    'matplotlib>=3.1.1'
 ]
 
 PACKAGE_DATA = {
