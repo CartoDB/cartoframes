f5ff53f31a2a248a017161981f94089c08f2b654
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2019-10-23 17:49:52 +0200

    Refactor DO namespaces

--- cartoframes/data/observatory/__init__.py
+++ cartoframes/data/observatory/__init__.py
@@ -7,6 +7,8 @@ from .dataset import CatalogDataset
 from .geography import Geography
 from .provider import Provider
 from .variable import Variable
+from .points_enrichment import enrich_points
+from .polygons_enrichment import enrich_polygons
 
 __all__ = [
     'Catalog',
@@ -15,5 +17,7 @@ __all__ = [
     'CatalogDataset',
     'Geography',
     'Provider',
-    'Variable'
+    'Variable',
+    'enrich_points',
+    'enrich_polygons'
 ]
