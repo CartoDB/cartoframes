ee04a5262ee4b1454fb8e0ae8ece23ba2c639b2e
Simon Martín <oleurud@gmail.com>
2019-11-08 19:25:34 +0100

    point test are unit. Also adding more cases

--- tests/e2e/data/observatory/enrichment/test_points_enrichment.py
+++ tests/e2e/data/observatory/enrichment/test_points_enrichment.py
@@ -1,11 +1,16 @@
 from cartoframes.auth import Credentials
 from cartoframes.data.clients.bigquery_client import BigQueryClient
-from cartoframes.data.observatory import Enrichment
+from cartoframes.data.observatory import Enrichment, Variable, CatalogDataset
 
 try:
-    from unittest.mock import Mock
+    from unittest.mock import Mock, patch
 except ImportError:
-    from mock import Mock
+    from mock import Mock, patch
+
+
+class CatalogEntityWithGeographyMock:
+    def __init__(self, geography):
+        self.geography = geography
 
 
 class TestPointsEnrichment(object):
@@ -22,35 +27,39 @@ class TestPointsEnrichment(object):
         self.username = None
         BigQueryClient._init_client = self.original_init_client
 
-    def test_enrichment_query_by_points_one_variable(self):
+    @patch.object(CatalogDataset, 'get')
+    def test_enrichment_query_by_points_one_variable(self, dataset_get_mock):
         enrichment = Enrichment(credentials=self.credentials)
-        username = self.username
+
         temp_table_name = 'test_table'
         data_geom_column = 'the_geom'
-        variables = enrichment._prepare_variables(
-          'carto-do.ags.demographics_crimerisk_usa_blockgroup_2015_yearly_2018.CRMCYBURG'
-        )
+        project = 'project'
+        dataset = 'dataset'
+        table = 'table'
+        variable_name = 'variable1'
+        column = 'column1'
+        geo_table = 'geo_table'
+        view = 'view_{}_{}'.format(dataset, table)
+        geo_view = 'view_{}_{}'.format(dataset, geo_table)
         filters = {'a': 'b'}
+        area_name = 'view_{}_{}_area'.format(dataset, table)
+
+        variable = Variable({
+            'id': '{}.{}.{}.{}'.format(project, dataset, table, variable_name),
+            'column_name': column,
+            'dataset_id': 'fake_name'
+        })
+        variables = [variable]
+
+        catalog = CatalogEntityWithGeographyMock('{}.{}.{}'.format(project, dataset, geo_table))
+        dataset_get_mock.return_value = catalog
 
         actual_queries = enrichment._prepare_points_enrichment_sql(
-            temp_table_name, data_geom_column, variables, filters)
+            temp_table_name, data_geom_column, variables, filters
+        )
 
         expected_queries = [
-            '''
-            SELECT
-                data_table.enrichment_id,
-                enrichment_table.CRMCYBURG,
-                ST_Area(enrichment_geo_table.geom)
-                    AS view_ags_demographics_crimerisk_usa_blockgroup_2015_yearly_2018_area
-            FROM `carto-do-customers.{username}\
-                .view_ags_demographics_crimerisk_usa_blockgroup_2015_yearly_2018` enrichment_table
-            JOIN `carto-do-customers.{username}\
-                .view_ags_geography_usa_blockgroup_2015` enrichment_geo_table
-            ON enrichment_table.geoid = enrichment_geo_table.geoid
-            JOIN `carto-do-customers.{username}.{temp_table_name}` data_table
-            ON ST_Within(data_table.{data_geom_column}, enrichment_geo_table.geom)
-            WHERE enrichment_table.a='b';
-            '''.format(username=username, temp_table_name=temp_table_name, data_geom_column=data_geom_column),
+            get_query([column], self.username, view, geo_view, temp_table_name, data_geom_column, area_name)
         ]
 
         actual = sorted(_clean_queries(actual_queries))
@@ -58,50 +67,149 @@ class TestPointsEnrichment(object):
 
         assert actual == expected
 
-    def test_enrichment_query_by_points_two_variables(self):
+    @patch.object(CatalogDataset, 'get')
+    def test_enrichment_query_by_points_two_variables(self, dataset_get_mock):
         enrichment = Enrichment(credentials=self.credentials)
-        username = self.username
+
         temp_table_name = 'test_table'
         data_geom_column = 'the_geom'
-        variables = enrichment._prepare_variables([
-            'carto-do.ags.demographics_crimerisk_usa_blockgroup_2015_yearly_2018.CRMCYBURG',
-            'carto-do.mastercard.financial_mrli_usa_blockgroup_2019_monthly_2019.ticket_size_score'
-        ])
+        project = 'project'
+        dataset = 'dataset'
+        table = 'table'
+        variable1_name = 'variable1'
+        variable2_name = 'variable2'
+        column1 = 'column1'
+        column2 = 'column2'
+        geo_table = 'geo_table'
+        view = 'view_{}_{}'.format(dataset, table)
+        geo_view = 'view_{}_{}'.format(dataset, geo_table)
         filters = {'a': 'b'}
+        area_name = 'view_{}_{}_area'.format(dataset, table)
+
+        variable1 = Variable({
+            'id': '{}.{}.{}.{}'.format(project, dataset, table, variable1_name),
+            'column_name': column1,
+            'dataset_id': 'fake_name'
+        })
+        variable2 = Variable({
+            'id': '{}.{}.{}.{}'.format(project, dataset, table, variable2_name),
+            'column_name': column2,
+            'dataset_id': 'fake_name'
+        })
+        variables = [variable1, variable2]
+
+        catalog = CatalogEntityWithGeographyMock('{}.{}.{}'.format(project, dataset, geo_table))
+        dataset_get_mock.return_value = catalog
 
         actual_queries = enrichment._prepare_points_enrichment_sql(
-            temp_table_name, data_geom_column, variables, filters)
+            temp_table_name, data_geom_column, variables, filters
+        )
 
         expected_queries = [
-            '''
-            SELECT
-                data_table.enrichment_id,
-                enrichment_table.CRMCYBURG,
-                ST_Area(enrichment_geo_table.geom)
-                    AS view_ags_demographics_crimerisk_usa_blockgroup_2015_yearly_2018_area
-            FROM `carto-do-customers.{username}\
-                .view_ags_demographics_crimerisk_usa_blockgroup_2015_yearly_2018` enrichment_table
-            JOIN `carto-do-customers.{username}\
-                .view_ags_geography_usa_blockgroup_2015` enrichment_geo_table
-            ON enrichment_table.geoid = enrichment_geo_table.geoid
-            JOIN `carto-do-customers.{username}.{temp_table_name}` data_table
-            ON ST_Within(data_table.{data_geom_column}, enrichment_geo_table.geom)
-            WHERE enrichment_table.a='b';
-            '''.format(username=username, temp_table_name=temp_table_name, data_geom_column=data_geom_column),
-            '''
-            SELECT data_table.enrichment_id,
-                enrichment_table.ticket_size_score,
-                ST_Area(enrichment_geo_table.geom)
-                    AS view_mastercard_financial_mrli_usa_blockgroup_2019_monthly_2019_area
-            FROM `carto-do-customers.{username}\
-                .view_mastercard_financial_mrli_usa_blockgroup_2019_monthly_2019` enrichment_table
-            JOIN `carto-do-customers.{username}\
-                .view_mastercard_geography_usa_blockgroup_2019` enrichment_geo_table
-            ON enrichment_table.geoid = enrichment_geo_table.geoid
-            JOIN `carto-do-customers.{username}.{temp_table_name}` data_table
-            ON ST_Within(data_table.{data_geom_column}, enrichment_geo_table.geom)
-            WHERE enrichment_table.a='b';
-            '''.format(username=username, temp_table_name=temp_table_name, data_geom_column=data_geom_column),
+            get_query([column1, column2], self.username, view, geo_view, temp_table_name, data_geom_column, area_name)
+        ]
+
+        actual = sorted(_clean_queries(actual_queries))
+        expected = sorted(_clean_queries(expected_queries))
+
+        assert actual == expected
+
+    @patch.object(CatalogDataset, 'get')
+    def test_enrichment_query_by_points_two_variables_different_tables(self, dataset_get_mock):
+        enrichment = Enrichment(credentials=self.credentials)
+
+        temp_table_name = 'test_table'
+        data_geom_column = 'the_geom'
+        project = 'project'
+        dataset = 'dataset'
+        table1 = 'table1'
+        table2 = 'table2'
+        variable1_name = 'variable1'
+        variable2_name = 'variable2'
+        column1 = 'column1'
+        column2 = 'column2'
+        geo_table = 'geo_table'
+        view1 = 'view_{}_{}'.format(dataset, table1)
+        view2 = 'view_{}_{}'.format(dataset, table2)
+        geo_view = 'view_{}_{}'.format(dataset, geo_table)
+        filters = {'a': 'b'}
+        area_name1 = 'view_{}_{}_area'.format(dataset, table1)
+        area_name2 = 'view_{}_{}_area'.format(dataset, table2)
+
+        variable1 = Variable({
+            'id': '{}.{}.{}.{}'.format(project, dataset, table1, variable1_name),
+            'column_name': column1,
+            'dataset_id': 'fake_name'
+        })
+        variable2 = Variable({
+            'id': '{}.{}.{}.{}'.format(project, dataset, table2, variable2_name),
+            'column_name': column2,
+            'dataset_id': 'fake_name'
+        })
+        variables = [variable1, variable2]
+
+        catalog = CatalogEntityWithGeographyMock('{}.{}.{}'.format(project, dataset, geo_table))
+        dataset_get_mock.return_value = catalog
+
+        actual_queries = enrichment._prepare_points_enrichment_sql(
+            temp_table_name, data_geom_column, variables, filters
+        )
+
+        expected_queries = [
+            get_query([column1], self.username, view1, geo_view, temp_table_name, data_geom_column, area_name1),
+            get_query([column2], self.username, view2, geo_view, temp_table_name, data_geom_column, area_name2)
+        ]
+
+        actual = sorted(_clean_queries(actual_queries))
+        expected = sorted(_clean_queries(expected_queries))
+
+        assert actual == expected
+
+    @patch.object(CatalogDataset, 'get')
+    def test_enrichment_query_by_points_two_variables_different_datasets(self, dataset_get_mock):
+        enrichment = Enrichment(credentials=self.credentials)
+
+        temp_table_name = 'test_table'
+        data_geom_column = 'the_geom'
+        project = 'project'
+        dataset1 = 'dataset1'
+        dataset2 = 'dataset2'
+        table1 = 'table1'
+        table2 = 'table2'
+        variable1_name = 'variable1'
+        variable2_name = 'variable2'
+        column1 = 'column1'
+        column2 = 'column2'
+        geo_table = 'geo_table'
+        view1 = 'view_{}_{}'.format(dataset1, table1)
+        view2 = 'view_{}_{}'.format(dataset2, table2)
+        geo_view = 'view_{}_{}'.format(dataset1, geo_table)
+        filters = {'a': 'b'}
+        area_name1 = 'view_{}_{}_area'.format(dataset1, table1)
+        area_name2 = 'view_{}_{}_area'.format(dataset2, table2)
+
+        variable1 = Variable({
+            'id': '{}.{}.{}.{}'.format(project, dataset1, table1, variable1_name),
+            'column_name': column1,
+            'dataset_id': 'fake_name'
+        })
+        variable2 = Variable({
+            'id': '{}.{}.{}.{}'.format(project, dataset2, table2, variable2_name),
+            'column_name': column2,
+            'dataset_id': 'fake_name'
+        })
+        variables = [variable1, variable2]
+
+        catalog = CatalogEntityWithGeographyMock('{}.{}.{}'.format(project, dataset1, geo_table))
+        dataset_get_mock.return_value = catalog
+
+        actual_queries = enrichment._prepare_points_enrichment_sql(
+            temp_table_name, data_geom_column, variables, filters
+        )
+
+        expected_queries = [
+            get_query([column1], self.username, view1, geo_view, temp_table_name, data_geom_column, area_name1),
+            get_query([column2], self.username, view2, geo_view, temp_table_name, data_geom_column, area_name2)
         ]
 
         actual = sorted(_clean_queries(actual_queries))
@@ -116,3 +224,30 @@ def _clean_queries(queries):
 
 def _clean_query(query):
     return query.replace('\n', '').replace(' ', '').lower()
+
+
+def get_query(columns, username, view, geo_table, temp_table_name, data_geom_column, area_name):
+    columns = ', '.join(get_column_sql(column) for column in columns)
+
+    return '''
+        SELECT data_table.enrichment_id, {columns}, ST_Area(enrichment_geo_table.geom) AS {area_name}
+        FROM `carto-do-customers.{username}.{view}` enrichment_table
+        JOIN `carto-do-customers.{username}.{geo_table}` enrichment_geo_table
+        ON enrichment_table.geoid = enrichment_geo_table.geoid
+        JOIN `carto-do-customers.{username}.{temp_table_name}` data_table
+        ON ST_Within(data_table.{data_geom_column}, enrichment_geo_table.geom)
+        WHERE enrichment_table.a='b';
+        '''.format(
+            columns=columns,
+            area_name=area_name,
+            username=username,
+            view=view,
+            geo_table=geo_table,
+            temp_table_name=temp_table_name,
+            data_geom_column=data_geom_column)
+
+
+def get_column_sql(column):
+    return '''
+        enrichment_table.{column}
+        '''.format(column=column)
