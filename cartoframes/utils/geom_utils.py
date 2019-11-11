1777fddd4fb5617d8a94c19f5b6ba0c0172ef68b
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2019-11-11 12:54:25 +0100

    Improve viz columns extraction: remove comments, findall

--- cartoframes/utils/geom_utils.py
+++ cartoframes/utils/geom_utils.py
@@ -270,8 +270,22 @@ def save_index_as_column(df):
 
 
 def extract_viz_columns(viz):
+    """Extract columns ($name) in viz"""
     columns = [RESERVED_GEO_COLUMN_NAME]
-    viz_columns = re.search(r'\$([a-z_]+)', viz)
+    viz_nocomments = remove_comments(viz)
+    viz_columns = re.findall(r'\$([a-z_]+)', viz_nocomments)
     if viz_columns is not None:
-        columns += list(viz_columns.groups())
+        columns += viz_columns
     return columns
+
+
+def remove_comments(text):
+    """Remove C-style comments"""
+    def replacer(match):
+        s = match.group(0)
+        return ' ' if s.startswith('/') else s
+    pattern = re.compile(
+        r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
+        re.DOTALL | re.MULTILINE
+    )
+    return re.sub(pattern, replacer, text)
