7754b8eb2d53535462912ff766b6af0451520a0f
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2019-11-14 15:30:46 +0100

    Fix columns detection in viz

--- cartoframes/utils/geom_utils.py
+++ cartoframes/utils/geom_utils.py
@@ -278,7 +278,7 @@ def extract_viz_columns(viz):
     """Extract columns ($name) in viz"""
     columns = [RESERVED_GEO_COLUMN_NAME]
     viz_nocomments = remove_comments(viz)
-    viz_columns = re.findall(r'\$([a-z_]+)', viz_nocomments)
+    viz_columns = re.findall(r'\$([A-Za-z0-9_]+)', viz_nocomments)
     if viz_columns is not None:
         columns += viz_columns
     return columns
@@ -293,4 +293,4 @@ def remove_comments(text):
         r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
         re.DOTALL | re.MULTILINE
     )
-    return re.sub(pattern, replacer, text)
+    return re.sub(pattern, replacer, text).strip()
