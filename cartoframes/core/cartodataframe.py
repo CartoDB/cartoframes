ec097d8ac4d4943205b8c0685662d3810b4223c8
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2019-11-19 15:16:22 +0100

    Wrap from_file and from_features methods in CDF

--- cartoframes/core/cartodataframe.py
+++ cartoframes/core/cartodataframe.py
@@ -13,15 +13,15 @@ class CartoDataFrame(GeoDataFrame):
         from ..io.carto import read_carto
         return read_carto(*args, **kwargs)
 
-    # @classmethod
-    # def from_file(cls, filename, **kwargs):
-    #     gdf = GeoDataFrame.from_file(filename, **kwargs)
-    #     return cls(gdf)
+    @classmethod
+    def from_file(cls, filename, **kwargs):
+        gdf = GeoDataFrame.from_file(filename, **kwargs)
+        return cls(gdf)
 
-    # @classmethod
-    # def from_features(cls, features, **kwargs):
-    #     gdf = GeoDataFrame.from_features(features, **kwargs)
-    #     return cls(gdf)
+    @classmethod
+    def from_features(cls, features, **kwargs):
+        gdf = GeoDataFrame.from_features(features, **kwargs)
+        return cls(gdf)
 
     def to_carto(self, *args, **kwargs):
         from ..io.carto import to_carto
