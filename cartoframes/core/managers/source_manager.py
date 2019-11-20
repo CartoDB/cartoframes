b663431c10f33b4984a6d01bc9c0937420eff4d6
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2019-11-20 11:05:12 +0100

    Implement SourceManager

--- cartoframes/core/managers/source_manager.py
+++ cartoframes/core/managers/source_manager.py
@@ -1,2 +1,41 @@
+from pandas import DataFrame
+
+from .context_manager import ContextManager
+
+
 class SourceManager(object):
-    pass
+
+    def __init__(self, source, credentials):
+        if isinstance(source, str):
+            # Table, SQL query
+            self._remote_data = True
+            self._context_manager = ContextManager(credentials)
+            self._query = self._context_manager.compute_query(source)
+        elif isinstance(source, DataFrame):
+            # DataFrame, GeoDataFrame, CartoDataFrame
+            self._remote_data = False
+            self._df = source
+        else:
+            raise ValueError('Wrong source input. Valid values are str and DataFrame.')
+
+    def is_remote(self):
+        return self._remote_data
+
+    def is_local(self):
+        return not self._remote_data
+
+    def get_query(self):
+        if self.is_remote():
+            return self._query
+
+    def get_num_rows(self):
+        if self.is_remote():
+            return self._context_manager.get_num_rows(self._query)
+        else:
+            return len(self._df)
+
+    def get_column_names(self):
+        if self.is_remote():
+            return self._context_manager.get_column_names(self._query)
+        else:
+            return list(self._df.columns)
