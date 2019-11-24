7263359cfafd7a14749e013fb0b98df4cf1c907b
Alberto Asuero <albertoasuero@gmail.com>
2019-11-24 18:34:31 +0100

    Removing tiletanic to give support to python 3.6

--- cartoframes/analysis/grid.py
+++ cartoframes/analysis/grid.py
@@ -1,7 +1,5 @@
-from tiletanic.tileschemes import WebMercator
-from tiletanic.tilecover import cover_geometry
+import mercantile
 from shapely.geometry import box
-import geopandas as gpd
 import pandas as pd
 from cartoframes import CartoDataFrame
 
@@ -9,33 +7,23 @@ from cartoframes import CartoDataFrame
 class QuadGrid():
 
     def polyfill(self, input_gdf, zoom_level):
-        tiler = WebMercator()
-        # Transform input to 3857
-        gdf = input_gdf.copy()
-        if not gdf.crs:
-            # assuming 4326 if not specified
-            gdf.crs = 'epsg:4326'
-        gdf = gdf.to_crs('epsg:3857')
 
         dfs = []
 
-        for _, row in gdf.iterrows():
-            tiles = cover_geometry(tiler, row['geometry'], zoom_level)
+        for _, row in input_gdf.iterrows():
+            bounds = row['geometry'].bounds
+            tiles = mercantile.tiles(bounds[0], bounds[1], bounds[2], bounds[3], zoom_level)
             resp = []
             for t in tiles:
                 r = row.copy()
-                r['geometry'] = box(*tiler.bbox(t))
-                r['quadkey'] = tiler.quadkey(t)
-                resp.append(r)
+                geometry = box(*mercantile.bounds(t))
+                if geometry.intersects(row['geometry']):
+                    r['geometry'] = geometry
+                    r['quadkey'] = mercantile.quadkey(t)
+                    resp.append(r)
 
             dfs.append(pd.DataFrame(resp))
 
-        r_gdf = CartoDataFrame(pd.concat(dfs).reset_index(drop=True))
-        r_gdf.crs = 'epsg:3857'
-        # Transforme to 4326
-        r_gdf = r_gdf.to_crs('epsg:4326')
-
-        # TODO: Fix it at CartoDataFrame class needs to reconvert to don't lose subclass.
-        r_gdf = CartoDataFrame(r_gdf)
-        r_gdf.crs = 'epsg:4326'
-        return r_gdf
+        cdf = CartoDataFrame(pd.concat(dfs).reset_index(drop=True))
+        cdf.crs = 'epsg:4326'
+        return cdf
--- tests/unit/analysis/test_grid.py
+++ tests/unit/analysis/test_grid.py
@@ -1,23 +1,76 @@
 # """Unit tests for cartoframes.analysis.grid"""
 
 import geopandas as gpd
-from shapely.geometry import box
+from shapely.geometry import box, shape
 from cartoframes.analysis.grid import QuadGrid
 from cartoframes import CartoDataFrame
 
+# DATA FRAME SRC BBOX
 pol_1 = box(1, 1, 2, 2)
 pol_2 = box(3, 3, 4, 4)
-GDF_SRC = gpd.GeoDataFrame({'id': [1, 2], 'geometry': [pol_1, pol_2]})
-GDF_SRC.crs = 'epsg:4326'
+GDF_BOX = gpd.GeoDataFrame({'id': [1, 2], 'geometry': [pol_1, pol_2]})
+
+pol_geojson = {
+    "type": "Polygon",
+    "coordinates": [
+        [
+            [
+                -5.899658203125,
+                38.436379603
+            ],
+            [
+                -6.690673828125,
+                37.67512527892127
+            ],
+            [
+                -6.15234375,
+                37.43997405227057
+            ],
+            [
+                -5.8447265625,
+                37.70120736474139
+            ],
+            [
+                -6.13037109375,
+                37.82280243352756
+            ],
+            [
+                -5.877685546874999,
+                38.02213147353745
+            ],
+            [
+                -6.009521484375,
+                38.12591462924157
+            ],
+            [
+                -5.5810546875,
+                38.1777509666256
+            ],
+            [
+                -5.899658203125,
+                38.436379603
+            ]
+        ]
+    ]
+}
+
+GDF_IRREGULAR = gpd.GeoDataFrame({'id': [1], 'geometry': [shape(pol_geojson)]})
 
 
 class TestGrid(object):
 
-    def test_quadgrid_polyfill(self, mocker):
+    def test_quadgrid_polyfill_box(self, mocker):
         """cartoframes.analysis.grid.QuadGrid.polyfill"""
-
-        cdf = QuadGrid().polyfill(GDF_SRC, 12)
+        cdf = QuadGrid().polyfill(GDF_BOX, 12)
         assert isinstance(cdf, gpd.GeoDataFrame)
         assert isinstance(cdf, CartoDataFrame)
         assert cdf.shape == (288, 3)
+        assert cdf.crs == 'epsg:4326'
+
+    def test_quadgrid_polyfill_pol(self, mocker):
+        """cartoframes.analysis.grid.QuadGrid.polyfill"""
+        cdf = QuadGrid().polyfill(GDF_IRREGULAR, 12)
+        assert isinstance(cdf, gpd.GeoDataFrame)
+        assert isinstance(cdf, CartoDataFrame)
+        assert cdf.shape == (98, 3)
         assert cdf.crs == 'epsg:4326'
