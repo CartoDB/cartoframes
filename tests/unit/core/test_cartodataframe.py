05d0e484610cee2442b7d94f3c40c272a23cf8b1
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2019-11-26 15:15:47 +0100

    Add filter test

--- tests/unit/core/test_cartodataframe.py
+++ tests/unit/core/test_cartodataframe.py
@@ -1,8 +1,10 @@
 """Unit tests for cartoframes.data."""
 
 import geopandas as gpd
-from cartoframes import CartoDataFrame
+
 from shapely.geometry import box
+
+from cartoframes import CartoDataFrame
 
 # DATA FRAME SRC BBOX
 pol_1 = box(1, 1, 2, 2)
@@ -12,15 +14,22 @@ GDF_BOX = gpd.GeoDataFrame({'id': [1, 2], 'geometry': [pol_1, pol_2]}, columns=[
 
 class TestCartoDataFrame(object):
 
-    def test_basic_inheritance(self, mocker):
+    def test_basic_inheritance(self):
         """Test basic inheritance"""
         cdf = CartoDataFrame(GDF_BOX)
         assert isinstance(cdf, CartoDataFrame)
 
-    def test_crs_inhericante(self, mocker):
-        """Test basic inheritance"""
+    def test_crs_inheritance(self):
+        """Test crs inheritance"""
         cdf = CartoDataFrame(GDF_BOX)
         cdf.crs = 'epsg:4326'
         cdf = cdf.to_crs('epsg:3857')
         assert isinstance(cdf, CartoDataFrame)
         assert cdf.crs == 'epsg:3857'
+
+    def test_filter_inheritance(self):
+        """Test filter inheritance"""
+        cdf = CartoDataFrame(GDF_BOX)
+        cdf = cdf[cdf.id > 1]
+        assert isinstance(cdf, CartoDataFrame)
+        assert len(cdf) == 1
