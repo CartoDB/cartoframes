5f0b19c13e082366690dd2dbfa34da5c1a66edea
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2019-11-29 10:53:19 +0100

    Refactor _init_clients

--- cartoframes/data/clients/bigquery_client.py
+++ cartoframes/data/clients/bigquery_client.py
@@ -15,12 +15,12 @@ from ...auth import get_default_credentials
 _USER_CONFIG_DIR = appdirs.user_config_dir('cartoframes')
 
 
-def refresh_client(func):
+def refresh_clients(func):
     def wrapper(self, *args, **kwargs):
         try:
             return func(self, *args, **kwargs)
         except RefreshError:
-            self.client = self._init_client()
+            self.bq_client, self.gcs_client = self._init_clients()
             try:
                 return func(self, *args, **kwargs)
             except RefreshError:
@@ -34,27 +34,23 @@ class BigQueryClient(object):
     def __init__(self, project, credentials):
         self._project = project
         self._credentials = credentials or get_default_credentials()
+        self._bucket = 'carto-do-{username}'.format(username=self._credentials.username)
+
+    def _init_clients(self):
         google_credentials = GoogleCredentials(self._credentials.get_do_token())
 
-        self.client = bigquery.Client(
+        bq_client = bigquery.Client(
             project=self._project,
             credentials=google_credentials)
 
-        self.gcs_client = storage.Client(
+        gcs_client = storage.Client(
             project=self._project,
             credentials=google_credentials
         )
 
-        self._bucket = 'carto-do-{username}'.format(username=self._credentials.username)
+        return bq_client, gcs_client
 
-    def _init_client(self):
-        google_credentials = GoogleCredentials(self._credentials.get_do_token())
-
-        return bigquery.Client(
-            project=self._project,
-            credentials=google_credentials)
-
-    @refresh_client
+    @refresh_clients
     def upload_dataframe(self, dataframe, schema, tablename, project, dataset):
 
         # Upload file to Google Cloud Storage
@@ -64,7 +60,7 @@ class BigQueryClient(object):
         blob.upload_from_filename(tablename)
 
         # Import from GCS To BigQuery
-        dataset_ref = self.client.dataset(dataset, project=project)
+        dataset_ref = self.bq_client.dataset(dataset, project=project)
         table_ref = dataset_ref.table(tablename)
         schema_wrapped = [bigquery.SchemaField(column, dtype) for column, dtype in schema.items()]
 
@@ -73,20 +69,20 @@ class BigQueryClient(object):
         job_config.source_format = bigquery.SourceFormat.CSV
         uri = 'gs://{bucket}/{tablename}'.format(bucket=self._bucket, tablename=tablename)
 
-        job = self.client.load_table_from_uri(
+        job = self.bq_client.load_table_from_uri(
             uri, table_ref, job_config=job_config
         )
 
         job.result()  # Waits for table load to complete.
 
-    @refresh_client
+    @refresh_clients
     def query(self, query, **kwargs):
-        return self.client.query(query, **kwargs)
+        return self.bq_client.query(query, **kwargs)
 
-    @refresh_client
+    @refresh_clients
     def get_table(self, project, dataset, table):
         full_table_name = '{}.{}.{}'.format(project, dataset, table)
-        return self.client.get_table(full_table_name)
+        return self.bq_client.get_table(full_table_name)
 
     def get_table_column_names(self, project, dataset, table):
         table_info = self.get_table(project, dataset, table)
