05bff0ec029ca6c2c05f7285d7971f0a1531cd78
Simon Martín <oleurud@gmail.com>
2019-12-11 18:13:27 +0100

    adding download unit test

--- tests/e2e/data/client/test_bigquery_client.py
+++ tests/e2e/data/client/test_bigquery_client.py
@@ -1,5 +1,4 @@
 import os
-import csv
 import json
 import pytest
 import unittest
@@ -10,11 +9,6 @@ from google.cloud import bigquery
 
 from cartoframes.auth import Credentials
 from cartoframes.data.clients.bigquery_client import BigQueryClient
-
-try:
-    from unittest.mock import Mock
-except ImportError:
-    from mock import Mock
 
 _WORKING_PROJECT = 'carto-do-customers'
 
@@ -91,40 +85,6 @@ class TestBigQueryClient(unittest.TestCase):
 
         bigquery.Client.query = original_query_method
 
-    def test_download_full(self):
-        data = [{'0': 'word', '1': 'word word'}]
-        columns = ['column1', 'column2']
-
-        original_query = BigQueryClient.query
-        BigQueryClient.query = Mock(return_value=QueryJobMock(data))
-        original_get_table_column_names = BigQueryClient.get_table_column_names
-        BigQueryClient.get_table_column_names = Mock(return_value=columns)
-
-        project = _WORKING_PROJECT
-        dataset = 'fake_dataset'
-        table = 'fake_table'
-        file_path = self.file_path
-
-        bq_client = BigQueryClient(project, self.credentials)
-
-        query = 'SELECT * FROM `{}.{}.{}`'.format(project, dataset, table)
-        job = bq_client.query(query)
-        file_path = bq_client.download_to_file(job, file_path=file_path, column_names=columns, progress_bar=False)
-
-        self.assertTrue(os.path.isfile(file_path))
-
-        rows = []
-        with open(file_path) as csvfile:
-            csvreader = csv.reader(csvfile)
-            rows.append(next(csvreader))
-            rows.append(next(csvreader))
-
-        self.assertEqual(rows[0], columns)
-        self.assertEqual(rows[1], list(data[0].values()))
-
-        BigQueryClient.query = original_query
-        BigQueryClient.get_table_column_names = original_get_table_column_names
-
     def test_download_using_if_exists(self):
         project = _WORKING_PROJECT
         dataset = 'fake_dataset'
@@ -138,5 +98,4 @@ class TestBigQueryClient(unittest.TestCase):
 
         with open(file_path, 'w'):
             with self.assertRaises(CartoException):
-                bq_client.download_to_file(job, file_path=file_path,
-                                           fail_if_exists=True, progress_bar=False)
+                bq_client.download_to_file(job, file_path=file_path, fail_if_exists=True, progress_bar=False)
