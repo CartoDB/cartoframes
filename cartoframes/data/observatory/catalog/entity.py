4bfa843ac854104aedc27fef47ed2ee01ada8806
Simon Martín <oleurud@gmail.com>
2019-12-19 14:51:22 +0100

    entity download to file or dataframe

--- cartoframes/data/observatory/catalog/entity.py
+++ cartoframes/data/observatory/catalog/entity.py
@@ -109,7 +109,7 @@ class CatalogEntity(ABC):
 
         return self.id
 
-    def _download(self, file_path, credentials):
+    def _download(self, credentials, file_path=None):
         if not self._is_available_in('bq'):
             raise CartoException('{} is not ready for Download. Please, contact us for more information.'.format(self))
 
@@ -128,12 +128,14 @@ class CatalogEntity(ABC):
             query = 'SELECT * FROM `{}`'.format(full_remote_table_name)
             job = bq_client.query(query)
 
-            bq_client.download_to_file(job, file_path, column_names=column_names)
+            if file_path:
+                bq_client.download_to_file(job, file_path, column_names=column_names)
+                log.info('Data saved: {}.'.format(file_path))
+                log.info("To read it you can do: `pandas.read_csv('{}')`.".format(file_path))
+            else:
+                return bq_client.download_to_dataframe(job)
         except NotFound:
             raise CartoException('You have not purchased the dataset `{}` yet'.format(self.id))
-
-        log.info('Data saved: {}.'.format(file_path))
-        log.info("To read it you can do: `pandas.read_csv('{}')`.".format(file_path))
 
     def _is_available_in(self, platform=_PLATFORM_BQ):
         return self.data['available_in'] and platform in self.data['available_in']
