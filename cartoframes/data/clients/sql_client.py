30e85f0e56ab6290c2ab993c5aaeb473e7796e58
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2021-03-11 14:45:49 +0100

    Review and improve SQLClient utilities

--- cartoframes/data/clients/sql_client.py
+++ cartoframes/data/clients/sql_client.py
@@ -3,6 +3,7 @@ from ...io.managers.context_manager import ContextManager
 
 class SQLClient:
     """SQLClient class is a client to run SQL queries in a CARTO account.
+    It also provides basic SQL utilities for analyzing and managing tables.
 
     Args:
         credentials (:py:class:`Credentials <cartoframes.auth.Credentials>`):
@@ -11,10 +12,6 @@ class SQLClient:
 
     Example:
         >>> sql = SQLClient(credentials)
-        >>> sql.query('SELECT * FROM table_name')
-        >>> sql.execute('DROP TABLE table_name')
-        >>> sql.distinct('table_name', 'column_name')
-        >>> sql.count('table_name')
 
     """
     def __init__(self, credentials=None):
@@ -30,6 +27,9 @@ class SQLClient:
         Args:
             query (str): SQL query.
             verbose (bool, optional): flag to return all the response. Default False.
+
+        Example:
+            >>> sql.query('SELECT * FROM table_name')
 
         """
         response = self._context_manager.execute_query(query.strip())
@@ -47,6 +47,9 @@ class SQLClient:
         Args:
             query (str): SQL query.
 
+        Example:
+            >>> sql.execute('DROP TABLE table_name')
+
         """
         return self._context_manager.execute_long_running_query(query.strip())
 
@@ -57,6 +60,10 @@ class SQLClient:
         Args:
             table_name (str): name of the table.
             column_name (str): name of the column.
+
+        Example:
+            >>> sql.distinct('table_name', 'column_name')
+            [('value1', 10), ('value2', 5)]
 
         """
         query = '''
@@ -72,16 +79,24 @@ class SQLClient:
         Args:
             table_name (str): name of the table.
 
+        Example:
+            >>> sql.count('table_name')
+            15
+
         """
         query = 'SELECT COUNT(*) FROM {};'.format(table_name)
         output = self.query(query)
         return output[0].get('count')
 
-    def bounds(self, query):
+    def bounds(self, table_name):
         """Get the bounds of the geometries in a table.
 
         Args:
-            query (str): SQL query containing a "the_geom" column.
+            table_name (str): name of the table containing a "the_geom" column.
+
+        Example:
+            >>> sql.bounds('table_name')
+            [[-1,-1], [1,1]]
 
         """
         query = '''
@@ -90,9 +105,9 @@ class SQLClient:
                 ARRAY[st_xmax(geom_env), st_ymax(geom_env)]
             ] bounds FROM (
                 SELECT ST_Extent(the_geom) geom_env
-                FROM ({}) q
+                FROM (SELECT the_geom FROM {}) q
             ) q;
-        '''.format(query)
+        '''.format(table_name)
         output = self.query(query)
         return output[0].get('bounds')
 
@@ -103,6 +118,16 @@ class SQLClient:
             table_name (str): name of the table.
             raw (bool, optional): return raw dict data if set to True.
                 Default False.
+
+        Example:
+            >>> sql.schema('table_name')
+            Column name          Column type
+            -------------------------------------
+            cartodb_id           number
+            the_geom             geometry
+            the_geom_webmercator geometry
+            column1              string
+            column2              number
 
         """
         query = 'SELECT * FROM {0} LIMIT 0;'.format(table_name)
@@ -125,6 +150,14 @@ class SQLClient:
             table_name (str): name of the table.
             column_name (str): name of the column.
 
+        Example:
+            >>> sql.schema('table_name', 'column_name')
+            count     1.00e+03
+            avg       2.00e+01
+            min       0.00e+00
+            max       5.00e+01
+            type: number
+
         """
         column_type = self._get_column_type(table_name, column_name)
         stats = ['COUNT(*)']
@@ -138,22 +171,26 @@ class SQLClient:
         '''.format(','.join(stats), table_name)
         output = self.query(query, verbose=True)
         fields = output.get('rows')[0]
-        rows = [(key, '{:0.2e}'.format(fields[key])) for key in fields]
+        rows = [(key, '{:0.2e}'.format(fields[key])) for key in fields if fields[key] is not None]
         self._print_table(rows, padding=[5, 10])
         print('type: {}'.format(column_type))
 
-    def create_table(self, table_name, columns, cartodbfy=True):
+    def create_table(self, table_name, column_names, column_types, cartodbfy=True):
         """Create a table with a specific table name and columns.
 
         Args:
             table_name (str): name of the table.
-            column_name (str): name of the column.
+            column_names (list of str): names of the columns.
+            column_types (list of str): types of the columns.
             cartodbfy (bool, optional): convert the table to CARTO format.
                 Default True. More info `here
                 <https://carto.com/developers/sql-api/guides/creating-tables/#create-tables>`.
 
+        Example:
+            >>> sql.create_table('table_name', ['column1', 'column2'], ['text', 'integer'])
+
         """
-        columns = ','.join(' '.join(x) for x in columns)
+        columns = [' '.join([name, column_types[i]]) for i, name in enumerate(column_names)]
         schema = self._context_manager.get_schema()
         query = '''
             BEGIN;
@@ -163,30 +200,29 @@ class SQLClient:
             COMMIT;
         '''.format(
             drop='DROP TABLE IF EXISTS {}'.format(table_name),
-            create='CREATE TABLE {0} ({1})'.format(table_name, columns),
+            create='CREATE TABLE {0} ({1})'.format(table_name, ','.join(columns)),
             cartodbfy='SELECT CDB_CartoDBFyTable(\'{0}\', \'{1}\')'.format(
                 schema, table_name) if cartodbfy else ''
         )
-        return self.execute(query)
+        self.execute(query)
 
     def insert_table(self, table_name, column_names, column_values):
         """Insert a row to the table.
 
         Args:
             table_name (str): name of the table.
-            column_names (str, list of str): names of the columns.
-            column_values (str, list of str): values of the columns.
+            column_names (list of str): names of the columns.
+            column_values (list of tuple): values of the columns.
+
+        Example:
+            >>> sql.insert_table('table_name', ['column1', 'column2'], [('value1', 1), ('value2', 2)])
 
         """
-        if isinstance(column_names, str):
-            column_names = [column_names]
-        if isinstance(column_values, str):
-            column_names = [column_values]
-        sql_values = [self._sql_format(x) for x in column_values]
+        sql_values = [self._row_values_format(row_values) for row_values in column_values]
         query = '''
-            INSERT INTO {0} ({1}) VALUES({2});
+            INSERT INTO {0} ({1}) VALUES {2};
         '''.format(table_name, ','.join(column_names), ','.join(sql_values))
-        return self.execute(query)
+        self.execute(query)
 
     def update_table(self, table_name, column_name, column_value, condition):
         """Update the column's value for the rows that match the condition.
@@ -197,12 +233,15 @@ class SQLClient:
             column_value (str): value of the column.
             condition (str): "where" condition of the request.
 
+        Example:
+            >>> sql.update_table('table_name', 'column1', 'VALUE1', 'column1=\'value1\'')
+
         """
         value = self._sql_format(column_value)
         query = '''
             UPDATE {0} SET {1}={2} WHERE {3};
         '''.format(table_name, column_name, value, condition)
-        return self.execute(query)
+        self.execute(query)
 
     def rename_table(self, table_name, new_table_name):
         """Rename a table from its table name.
@@ -211,9 +250,12 @@ class SQLClient:
             table_name (str): name of the original table.
             new_table_name (str): name of the new table.
 
+        Example:
+            >>> sql.rename_table('table_name', 'table_name2')
+
         """
         query = 'ALTER TABLE {0} RENAME TO {1};'.format(table_name, new_table_name)
-        return self.execute(query)
+        self.execute(query)
 
     def drop_table(self, table_name):
         """Remove a table from its table name.
@@ -221,9 +263,12 @@ class SQLClient:
         Args:
             table_name (str): name of the table.
 
+        Example:
+            >>> sql.drop_table('table_name')
+
         """
         query = 'DROP TABLE IF EXISTS {0};'.format(table_name)
-        return self.execute(query)
+        self.execute(query)
 
     def _get_column_type(self, table_name, column_name):
         query = 'SELECT {0} FROM {1} LIMIT 0;'.format(column_name, table_name)
@@ -238,6 +283,9 @@ class SQLClient:
         if isinstance(value, bool):
             return 'TRUE' if value else 'FALSE'
         return str(value)
+
+    def _row_values_format(self, row_values):
+        return '({})'.format(','.join([self._sql_format(value) for value in row_values]))
 
     def _print_table(self, rows, columns=None, padding=None):
         row_format = ''
